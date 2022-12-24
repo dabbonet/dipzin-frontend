7d47e87c6a75030dcb856469b086b687ad8f34e8
Ahmed Mahmoud <Ahmedumahmoud@gmail.com>
2022-12-24 16:49:12 +0200

    OG Image final

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -9,26 +9,17 @@ export default {
         siteName: 'dipzin',
         images: [
             {
-                url: 'https://www.dipzin.com/images/og/og-image-01.jpg',
-                width: 800,
-                height: 600,
-                alt: 'Your Go-To Source for Digital Inspiration ',
-                type: 'image/jpeg',
-            },
-            {
-                url: 'https://www.dipzin.com/images/og/og-image-02.jpg',
-                width: 900,
-                height: 800,
-                alt: 'Your Go-To Source for Digital Inspiration',
-                type: 'image/jpeg',
-            },
-            {
-                url: 'https://www.dipzin.com/images/og/og-image-fb.jpg',
+                url: 'https://www.dipzin.com/images/og/og-image.jpg',
                 width: 1200,
                 height: 630,
                 alt: 'Your Go-To Source for Digital Inspiration',
                 type: 'image/jpeg',
             }
         ],
+    },
+    twitter: {
+        handle: '@dipzincom',
+        site: '@dipzincom',
+        cardType: 'summary_large_image',
     }
 };
