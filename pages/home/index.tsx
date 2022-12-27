c4d23620b22bc6455d9fb7db807b3383c5bac0fc
Flepoo <Mohamedhoshame@gmail.com>
2022-12-27 18:56:01 +0200

    add home

---
+++ pages/home/index.tsx
@@ -0,0 +1,110 @@
+import { NextPage } from "next";
+const Page: NextPage = () => {
+    return(
+        <>
+            <header className="w-full flex justify-between fixed items-center text-white mt-8 px-10">
+                <div><span>dipzin.</span></div>
+                <div><span>Paltform IOS</span></div>
+                <div><span>Try it!</span></div>
+            </header>
+
+            <main className="w-full flex flex-col items-center">
+                <div className="min-w-[75%] h-[275px] bg-gradient-to-b from-[#0891B2] to-blue-700 mt-18 border-r-5 mt-[90px] rounded-[42px]" ></div>
+                <div className="min-w-[75%] flex mt-10 mb-[55px]">
+                    <a className="cursor-pointer"><span className="text-white text-5xl font-light">Stream</span></a>
+                    <a className="cursor-pointer"><span className="text-gray-400 text-5xl ml-12 opacity-70">Collections</span></a>
+                </div>
+                <div className="w-[75%] grid grid-cols-6 gap-y-2">
+
+                    <div className="w-[145px] h-[314px]  flex justify-center items-center cursor-pointer">
+                        <div className="w-[94%] h-[94%] bg-slate-500 rounded-2xl overflow-hidden flex justify-center items-center">
+                            <img src="https://mobbin.com/_next/image?url=https%3A%2F%2Fujasntkfphywizsdaapi.supabase.co%2Fstorage%2Fv1%2Fobject%2Fpublic%2Fcontent%2Fapp_screens%2F4cd2000b-5712-461b-ac15-79d29aae7bf6.png&w=360&q=85"/>
+                        </div>
+                    </div>
+
+                    <div className="w-[145px] h-[314px]  flex justify-center items-center">
+                        <div className="w-[95%] h-[95%] bg-slate-500 rounded-2xl overflow-hidden">
+                            <img src="https://mobbin.com/_next/image?url=https%3A%2F%2Fujasntkfphywizsdaapi.supabase.co%2Fstorage%2Fv1%2Fobject%2Fpublic%2Fcontent%2Fapp_screens%2F4cd2000b-5712-461b-ac15-79d29aae7bf6.png&w=360&q=85"/>
+                        </div>
+                    </div>
+                    <div className="w-[145px] h-[314px]  flex justify-center items-center">
+                        <div className="w-[95%] h-[95%] bg-slate-500 rounded-2xl overflow-hidden">
+                            <img src="https://mobbin.com/_next/image?url=https%3A%2F%2Fujasntkfphywizsdaapi.supabase.co%2Fstorage%2Fv1%2Fobject%2Fpublic%2Fcontent%2Fapp_screens%2F28bdc871-04fc-4591-be32-c4c585d1193c.png&w=3840&q=85"/>
+                        </div>
+                    </div>
+                    <div className="w-[145px] h-[314px]  flex justify-center items-center">
+                        <div className="w-[95%] h-[95%] bg-slate-500 rounded-2xl overflow-hidden">
+                            <img src="https://mobbin.com/_next/image?url=https%3A%2F%2Fujasntkfphywizsdaapi.supabase.co%2Fstorage%2Fv1%2Fobject%2Fpublic%2Fcontent%2Fapp_screens%2F4cd2000b-5712-461b-ac15-79d29aae7bf6.png&w=360&q=85"/>
+                        </div>
+                    </div>
+                    <div className="w-[145px] h-[314px]  flex justify-center items-center">
+                        <div className="w-[95%] h-[95%] bg-slate-500 rounded-2xl overflow-hidden">
+                            <img src="https://mobbin.com/_next/image?url=https%3A%2F%2Fujasntkfphywizsdaapi.supabase.co%2Fstorage%2Fv1%2Fobject%2Fpublic%2Fcontent%2Fapp_screens%2F28bdc871-04fc-4591-be32-c4c585d1193c.png&w=3840&q=85"/>
+                        </div>
+                    </div>
+                    <div className="w-[145px] h-[314px]  flex justify-center items-center">
+                        <div className="w-[95%] h-[95%] bg-slate-500 rounded-2xl overflow-hidden">
+                            <img src="https://mobbin.com/_next/image?url=https%3A%2F%2Fujasntkfphywizsdaapi.supabase.co%2Fstorage%2Fv1%2Fobject%2Fpublic%2Fcontent%2Fapp_screens%2F4cd2000b-5712-461b-ac15-79d29aae7bf6.png&w=360&q=85"/>
+                        </div>
+                    </div>
+                    <div className="w-[145px] h-[314px]  flex justify-center items-center">
+                        <div className="w-[95%] h-[95%] bg-slate-500 rounded-2xl overflow-hidden">
+                            <img src="https://mobbin.com/_next/image?url=https%3A%2F%2Fujasntkfphywizsdaapi.supabase.co%2Fstorage%2Fv1%2Fobject%2Fpublic%2Fcontent%2Fapp_screens%2F28bdc871-04fc-4591-be32-c4c585d1193c.png&w=3840&q=85"/>
+                        </div>
+                    </div>
+                    <div className="w-[145px] h-[314px]  flex justify-center items-center">
+                        <div className="w-[95%] h-[95%] bg-slate-500 rounded-2xl overflow-hidden">
+                            <img src="https://mobbin.com/_next/image?url=https%3A%2F%2Fujasntkfphywizsdaapi.supabase.co%2Fstorage%2Fv1%2Fobject%2Fpublic%2Fcontent%2Fapp_screens%2F4cd2000b-5712-461b-ac15-79d29aae7bf6.png&w=360&q=85"/>
+                        </div>
+                    </div>
+                    <div className="w-[145px] h-[314px]  flex justify-center items-center">
+                        <div className="w-[95%] h-[95%] bg-slate-500 rounded-2xl overflow-hidden">
+                            <img src="https://mobbin.com/_next/image?url=https%3A%2F%2Fujasntkfphywizsdaapi.supabase.co%2Fstorage%2Fv1%2Fobject%2Fpublic%2Fcontent%2Fapp_screens%2F28bdc871-04fc-4591-be32-c4c585d1193c.png&w=3840&q=85"/>
+                        </div>
+                    </div>
+                    <div className="w-[145px] h-[314px]  flex justify-center items-center">
+                        <div className="w-[95%] h-[95%] bg-slate-500 rounded-2xl overflow-hidden">
+                            <img src="https://mobbin.com/_next/image?url=https%3A%2F%2Fujasntkfphywizsdaapi.supabase.co%2Fstorage%2Fv1%2Fobject%2Fpublic%2Fcontent%2Fapp_screens%2F4cd2000b-5712-461b-ac15-79d29aae7bf6.png&w=360&q=85"/>
+                        </div>
+                    </div>
+                    <div className="w-[145px] h-[314px]  flex justify-center items-center">
+                        <div className="w-[95%] h-[95%] bg-slate-500 rounded-2xl overflow-hidden">
+                            <img src="https://mobbin.com/_next/image?url=https%3A%2F%2Fujasntkfphywizsdaapi.supabase.co%2Fstorage%2Fv1%2Fobject%2Fpublic%2Fcontent%2Fapp_screens%2F28bdc871-04fc-4591-be32-c4c585d1193c.png&w=3840&q=85"/>
+                        </div>
+                    </div>
+                    <div className="w-[145px] h-[314px]  flex justify-center items-center">
+                        <div className="w-[95%] h-[95%] bg-slate-500 rounded-2xl overflow-hidden">
+                            <img src="https://mobbin.com/_next/image?url=https%3A%2F%2Fujasntkfphywizsdaapi.supabase.co%2Fstorage%2Fv1%2Fobject%2Fpublic%2Fcontent%2Fapp_screens%2F4cd2000b-5712-461b-ac15-79d29aae7bf6.png&w=360&q=85"/>
+                        </div>
+                    </div>
+                    <div className="w-[145px] h-[314px]  flex justify-center items-center">
+                        <div className="w-[95%] h-[95%] bg-slate-500 rounded-2xl overflow-hidden">
+                            <img src="https://mobbin.com/_next/image?url=https%3A%2F%2Fujasntkfphywizsdaapi.supabase.co%2Fstorage%2Fv1%2Fobject%2Fpublic%2Fcontent%2Fapp_screens%2F28bdc871-04fc-4591-be32-c4c585d1193c.png&w=3840&q=85"/>
+                        </div>
+                    </div>
+                    <div className="w-[145px] h-[314px]  flex justify-center items-center">
+                        <div className="w-[95%] h-[95%] bg-slate-500 rounded-2xl overflow-hidden">
+                            <img src="https://mobbin.com/_next/image?url=https%3A%2F%2Fujasntkfphywizsdaapi.supabase.co%2Fstorage%2Fv1%2Fobject%2Fpublic%2Fcontent%2Fapp_screens%2F4cd2000b-5712-461b-ac15-79d29aae7bf6.png&w=360&q=85"/>
+                        </div>
+                    </div>
+                    <div className="w-[145px] h-[314px]  flex justify-center items-center">
+                        <div className="w-[95%] h-[95%] bg-slate-500 rounded-2xl overflow-hidden">
+                            <img src="https://mobbin.com/_next/image?url=https%3A%2F%2Fujasntkfphywizsdaapi.supabase.co%2Fstorage%2Fv1%2Fobject%2Fpublic%2Fcontent%2Fapp_screens%2F28bdc871-04fc-4591-be32-c4c585d1193c.png&w=3840&q=85"/>
+                        </div>
+                    </div>
+                    <div className="w-[145px] h-[314px]  flex justify-center items-center">
+                        <div className="w-[95%] h-[95%] bg-slate-500 rounded-2xl overflow-hidden">
+                            <img src="https://mobbin.com/_next/image?url=https%3A%2F%2Fujasntkfphywizsdaapi.supabase.co%2Fstorage%2Fv1%2Fobject%2Fpublic%2Fcontent%2Fapp_screens%2F4cd2000b-5712-461b-ac15-79d29aae7bf6.png&w=360&q=85"/>
+                        </div>
+                    </div>
+
+
+
+
+
+                </div>
+            </main>
+        </>
+    );
+}
+export default Page;
