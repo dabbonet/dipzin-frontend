4283746078253778875a45068d0f822b5022c06d
Ahmed Mahmoud <Ahmedumahmoud@gmail.com>
2022-12-28 16:02:38 +0200

    update screen v3

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -4,24 +4,24 @@ const Page: NextPage = () => {
     const [platform, setPlatform] = useState<any>("IOS");
 
     const handlePlateformUp = () => {
-        if(platform == "IOS"){
+        if (platform == "IOS") {
             setPlatform("Android");
-        } else if(platform == "Android"){
+        } else if (platform == "Android") {
             setPlatform("Web");
-        } else if(platform == "Web"){
+        } else if (platform == "Web") {
             setPlatform("IOS");
         }
-    } 
+    }
 
     const handlePlateformDown = () => {
-        if(platform == "Web"){
+        if (platform == "Web") {
             setPlatform("Android");
-        } else if(platform == "Android"){
+        } else if (platform == "Android") {
             setPlatform("IOS");
-        } else if(platform == "IOS"){
+        } else if (platform == "IOS") {
             setPlatform("Web");
         }
-    } 
+    }
 
     return (
         <>
@@ -54,7 +54,7 @@ const Page: NextPage = () => {
                         <div className="rounded-2xl overflow-hidden min-720:gap-16 sm:gap-5 transform transition duration-500 hover:scale-105">
                             <img className="h-full w-full border-[2px] border-transparent rounded-2xl transform opacity duration-500 hover:border-slate-300" src="https://mobbin.com/_next/image?url=https%3A%2F%2Fujasntkfphywizsdaapi.supabase.co%2Fstorage%2Fv1%2Fobject%2Fpublic%2Fcontent%2Fapp_screens%2F4cd2000b-5712-461b-ac15-79d29aae7bf6.png&w=360&q=85" />
                             <div className="absolute w-[100%] top-8 flex justify-center drop-shadow-xl">
-                                <img className="h-[50%] w-full" src="/images/assets/addpng.svg" />
+                                <img src="/images/assets/addpng.svg" />
                                 <img src="/images/assets/addcopy.svg" />
                                 <img src="/images/assets/addcollection.svg" />
                             </div>
@@ -72,17 +72,17 @@ const Page: NextPage = () => {
                         <div className="rounded-2xl overflow-hidden min-720:gap-16 transform transition duration-500 hover:scale-105">
                             <img className="h-full w-full border-[3px] border-transparent rounded-2xl transform opacity duration-500 hover:border-slate-300" src="https://mobbin.com/_next/image?url=https%3A%2F%2Fujasntkfphywizsdaapi.supabase.co%2Fstorage%2Fv1%2Fobject%2Fpublic%2Fcontent%2Fapp_screens%2Fece20f10-696c-432b-a2e9-d22116e20344.png&w=1440&q=85" />
                             <div className="absolute w-[100%] top-4 flex justify-center drop-shadow-xl">
-                                <img className=" h-[30%] w-[30%] transform transition duration-500 hover:scale-110 cursor-pointer" src="/images/assets/addpng.svg" />
-                                <img className=" h-[30%] w-[30%] transform transition duration-500 hover:scale-110 cursor-pointer" src="/images/assets/addcopy.svg" />
-                                <img className=" h-[30%] w-[30%] transform transition duration-500 hover:scale-110 cursor-pointer" src="/images/assets/addcollection.svg" />
+                                <img className=" h-[27%] w-[27%] transform transition duration-500 hover:scale-110 cursor-pointer" src="/images/assets/addpng.svg" />
+                                <img className=" h-[27%] w-[27%] transform transition duration-500 hover:scale-110 cursor-pointer" src="/images/assets/addcopy.svg" />
+                                <img className=" h-[27%] w-[27%] transform transition duration-500 hover:scale-110 cursor-pointer" src="/images/assets/addcollection.svg" />
                             </div>
                             <div className="absolute w-[100%] bottom-5 flex justify-start drop-shadow-xl" >
-                                <img className="h-[20%] w-[25%] ml-[4%] rounded-full" src="/images/assets/appicon.svg" />
+                                <img className="h-[20%] w-[20%] ml-[4%] rounded-full cursor-pointer" src="/images/assets/appicon.svg" />
                                 <div className="text-white">
-                                    <span className="ml-1 text-[16px] font-semibold">Hollister</span>
-                                    <span className="block text-[8px] font-light ml-1">Fashion & Fitness</span>
+                                    <span className="ml-2 text-[16px] font-semibold">Hollister</span>
+                                    <span className="block text-[8px] font-light ml-2">Fashion & Fitness</span>
                                 </div>
-                                <img className="ml-auto mr-[4%] h-[20%] w-[25%]" src="/images/assets/screenzome.svg" />
+                                <img className="ml-auto mr-[4%] h-[20%] w-[20%] cursor-pointer" src="/images/assets/screenzome.svg" />
 
                             </div>
                         </div>
