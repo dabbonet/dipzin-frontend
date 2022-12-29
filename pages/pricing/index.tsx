638bd49a29b9aa75b33812bdf1d554f9a6c2d496
Ahmed Mahmoud <Ahmedumahmoud@gmail.com>
2022-12-29 22:19:25 +0200

    finish pricing without logic

--- pages/pricing/index.tsx
+++ pages/pricing/index.tsx
@@ -81,10 +81,22 @@ const Page: NextPage = () => {
                     <span className="text-[20px] text-slate-300">We believe Untitled should be accessible to all companies, no matter the size.</span>
                 </div>
 
-                <div className="w-[930px] h-[60px] bg-slate-800 rounded-2xl flex items-center">
-                    <div className="w-[20%] h-[70%] bg-white rounded-xl ml-3 mr-auto"></div>
-                    <div className="w-[20%] h-[70%] bg-white rounded-xl ml-auto mr-auto"></div>
-                    <div className="w-[20%] h-[70%] bg-white rounded-xl ml-auto mr-3"></div>
+                <div className="w-[930px] py-3 bg-slate-800 rounded-2xl flex items-center">
+                    <div className="px-10 h-[45px] bg-slate-900 flex justify-center items-center rounded-xl ml-3 mr-auto">
+                        <span className="font-semibold text-slate-200 text-[20px]">MONTHLY</span>
+                    </div>
+                    <div className="pl-10 pr-5 h-[45px] bg-orange-500 flex justify-center items-center rounded-xl ml-auto mr-auto">
+                    <span className="font-semibold text-slate-200 text-[20px]">QUARTERLY</span>
+                       <div className="h-7 bg-orange-200 flex justify-center items-center rounded-lg px-3 ml-5 mr-auto">
+                            <span className="text-orange-700 font-semibold text-[14px]">Save 35%</span>
+                        </div>
+                    </div>
+                    <div className="pl-10 pr-5 h-[45px] bg-slate-900 flex justify-center items-center rounded-xl ml-auto mr-3">
+                        <span className="font-semibold text-slate-200 text-[20px]">ANNUALLY</span>
+                        <div className="h-7 bg-emerald-200 flex justify-center items-center rounded-lg px-3 ml-5 mr-auto">
+                            <span className="text-emerald-700 font-semibold text-[14px]">Save 35%</span>
+                        </div>
+                    </div>
                 </div>
 
                 <div className=" grid grid-cols-2 gap-5 my-12 gap-x-10">
@@ -108,7 +120,7 @@ const Page: NextPage = () => {
                             </div>
 
                         </div>
-                        <div className="flex mt-auto items-center justify-center w-[100%] h-[42px] bg-[#0B1321] mx-auto rounded-[26px]">
+                        <div className="flex mt-auto items-center justify-center w-[100%] h-[52px] bg-[#0B1321] mx-auto rounded-[26px]">
                             <span>Get Started</span>
                         </div>
                     </div>
@@ -136,27 +148,67 @@ const Page: NextPage = () => {
                                 <span className="font-medium text-[19px] ml-1">Unlimited Search & Filters</span>
                             </div>
                         </div>
-                        <div className="flex items-center justify-center w-[100%] h-[42px] bg-[#0B1321] mx-auto rounded-[26px]">
+                        <div className="flex items-center justify-center w-[100%] h-[52px] bg-[#0B1321] mx-auto rounded-[26px]">
                             <span>Get Started</span>
                         </div>
                     </div>
                 </div>
 
-                <div className="flex flex-col">
-                    <span>Features</span>
-                    <span>Beautiful analytics to grow smarter</span>
-                    <span>Powerful, self-serve product and growth analytics to help you convert, </span>
-                    <span>engage, and retain more users. Trusted by over 4,000 startups. </span>
+                <div className="flex flex-col w-[75%] mt-10 mb-10">
+                    <span className="text-orange-500 font-semibold text-[16px] mb-3">Features</span>
+                    <span className="text-[36px] mb-3">Beautiful analytics to grow smarter</span>
+                    <span className="text-[#949DAD] text-[20px]">Powerful, self-serve product and growth analytics to help you convert, </span>
+                    <span className="text-[#949DAD] text-[20px]">engage, and retain more users. Trusted by over 4,000 startups. </span>
                 </div>
-                <div className="grid grid-cols-3 gap-5 ">
+
+                <div className="grid grid-cols-3 gap-y-[80px] gap-x-[80px] mx-[12.5%] my-12">
+
+                    <div>
+                        <div className="mb-5"><img src="/images/assets/priconone.svg" /></div>
+                        <span className="font-medium text-[20px] flex my-2.5">Share team inboxes</span>
+                        <span className="text-[16px] text-[#667085]">Whether you have a team of 2 or 200, our shared team inboxes keep everyone on the same page and in the loop.</span>
+                        <span className="block text-[16px] text-orange-500 mt-5">Learn more</span>
+                    </div>
+                    <div>
+                        <div className="mb-5"><img src="/images/assets/pricon2.svg" /></div>
+                        <span className="font-medium text-[20px] flex my-2.5">Deliver instant answers</span>
+                        <span className="text-[16px] text-[#667085]">An all-in-one customer service platform that helps you balance everything your customers need to be happy.</span>
+                        <span className="block text-[16px] text-orange-500 mt-5">Learn more</span>
+                    </div>
+                    <div>
+                        <div className="mb-5"><img src="/images/assets/pricon3.svg" /></div>
+                        <span className="font-medium text-[20px] flex my-2.5">Manage your team with reports</span>
+                        <span className="text-[16px] text-[#667085]">Measure what matters with Untitled’s easy-to-use reports. You can filter, export, and drilldown on the data in a couple clicks.</span>
+                        <span className="block text-[16px] text-orange-500 mt-5">Learn more</span>
+                    </div>
+                    <div>
+                        <div className="mb-5"><img src="/images/assets/pricon4.svg" /></div>
+                        <span className="font-medium text-[20px] flex my-2.5">Connect with customers</span>
+                        <span className="text-[16px] text-[#667085]">Solve a problem or close a sale in real-time with chat. If no one is available, customers are seamlessly routed to email without confusion.</span>
+                        <span className="block text-[16px] text-orange-500 mt-5">Learn more</span>
+                    </div>
+                    <div>
+                        <div className="mb-5"><img src="/images/assets/pricon5.svg" /></div>
+                        <span className="font-medium text-[20px] flex my-2.5">Connect the tools you already use</span>
+                        <span className="text-[16px] text-[#667085]">Explore 100+ integrations that make your day-to-day workflow more efficient and familiar. Plus, our extensive developer tools.</span>
+                        <span className="block text-[16px] text-orange-500 mt-5">Learn more</span>
+                    </div>
+                    <div>
+                        <div className="mb-5"><img src="/images/assets/pricon6.svg" /></div>
+                        <span className="font-medium text-[20px] flex my-2.5">Our people make the difference</span>
+                        <span className="text-[16px] text-[#667085]">We’re an extension of your customer service team, and all of our resources are free. Chat to our friendly team 24/7 when you need help.</span>
+                        <span className="block text-[16px] text-orange-500 mt-5">Learn more</span>
+                    </div>
 
                 </div>
-                <div className="flex flex-col">
-                    <span>Features</span>
-                    <span>Beautiful analytics to grow smarter</span>
-                    <span>Powerful, self-serve product and growth analytics to help you convert, </span>
-                    <span>engage, and retain more users. Trusted by over 4,000 startups. </span>
+
+                <div className="flex flex-col w-[75%] mt-10 mb-10">
+                    <span className="text-orange-500 font-semibold text-[16px] mb-3">Features</span>
+                    <span className="text-[36px] mb-3">Beautiful analytics to grow smarter</span>
+                    <span className="text-[#949DAD] text-[20px]">Powerful, self-serve product and growth analytics to help you convert, </span>
+                    <span className="text-[#949DAD] text-[20px]">engage, and retain more users. Trusted by over 4,000 startups. </span>
                 </div>
+
             </main>
 
         </>
