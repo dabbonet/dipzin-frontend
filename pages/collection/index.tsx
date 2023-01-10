b8afa8dbe82ae8f422b39bb1da0bf032bfe5f567
Ahmed Mahmoud <Ahmedumahmoud@gmail.com>
2023-01-11 00:57:39 +0200

    Collection Grid - Success 👌

--- pages/collection/index.tsx
+++ pages/collection/index.tsx
@@ -5,13 +5,11 @@ const Collections = () => {
     return (
         <div className="w-[80%] lg:w-[75%] grid lg:grid-cols-4 lg:gap-5 gap-5 mb-10 grid-cols-1 pb-32">
             <CollectionCard />
-            {/* <CollectionCard />
             <CollectionCard />
             <CollectionCard />
             <CollectionCard />
             <CollectionCard />
             <CollectionCard />
-            <CollectionCard /> */}
         </div>
     )
 }
