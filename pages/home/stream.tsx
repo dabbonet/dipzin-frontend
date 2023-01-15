02e6103bc318c601b322407554cda5a288e7ef3e
Ahmed Mahmoud <Ahmedumahmoud@gmail.com>
2023-01-15 21:25:02 +0200

    Fix infinite scroll, limited to 50 fetches Supabase repeating apps ( BUG )

--- pages/home/stream.tsx
+++ pages/home/stream.tsx
@@ -1,10 +1,11 @@
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useQuery, UseQueryResult, useInfiniteQuery } from "react-query";
 import Screen from "../../components/screen";
 
 
 export const fetchStream = async () => {
+    console.log('fetching...');
     const res = await fetch('/api/stream');
 
     if (res.ok) {
@@ -16,34 +17,38 @@ export const fetchStream = async () => {
 
 
 const Stream = () => {
-
+    const [totalFetched, setTotalFetched] = useState(0);
     const { isLoading, isError, data, error, hasNextPage, fetchNextPage, fetchPreviousPage } = useInfiniteQuery(
         'stream',
         () => fetchStream(),
         {
-            getNextPageParam: (lastPage) => {
-                // lastPage signature depends on your api respond, below is a pseudocode
-                if (lastPage.hasNextPage) {
-                    return lastPage.nextCursor;
+            getNextPageParam: (lastGroup, allGroups) => {
+                if (totalFetched < 50) {
+                    return lastGroup.length;
                 }
                 return undefined;
             },
             keepPreviousData: true,
+            refetchOnWindowFocus: false,
         }
     );
 
     useEffect(() => {
         let fetching = false;
         const onScroll = async (event) => {
+
             const { scrollHeight, scrollTop, clientHeight } =
                 event.target.scrollingElement;
 
             if (!fetching && scrollHeight - scrollTop <= clientHeight * 1.5) {
-                fetching = true;
-                // console.log('scroll');
+                if (totalFetched < 50) {
 
-                console.log(data)
-                fetching = false;
+                    fetching = true;
+                    await fetchNextPage();
+                    setTotalFetched(totalFetched + 1);
+                    fetching = false;
+
+                }
             }
         };
 
@@ -51,7 +56,7 @@ const Stream = () => {
         return () => {
             document.removeEventListener("scroll", onScroll);
         };
-    }, []);
+    }, [totalFetched]);
 
     // console.log(data.pages[0]);
 
@@ -61,7 +66,7 @@ const Stream = () => {
     return (
         <div className="w-[80%] lg:w-[75%] grid gap-4 lg:gap-5 xl:gap-6 xxl:gap-9 grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 xxl:grid-cols-7 mb-10 text-white">
             {data?.pages.map((page) =>
-                data?.pages[0].map((application: any) => (
+                page.map((application: any) => (
                     <>
 
                         <Screen key={application.id} platform={1} app={application} list={application.showcase} />
