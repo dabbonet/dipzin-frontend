f63629d16314703a530fe9edeebbdcca610e139e
Ahmed Mahmoud <Ahmedumahmoud@gmail.com>
2023-01-17 17:07:27 +0200

    strream new login v0

--- pages/api/stream/index.ts
+++ pages/api/stream/index.ts
@@ -1,14 +1,20 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import supabase from '../../../lib/supabase';
 
-export default (_req: NextApiRequest, res: NextApiResponse): void => {
+export default (req: NextApiRequest, res: NextApiResponse): void => {
+    const page = parseInt(req.query.page) || 1;
+    const perPage = parseInt(req.query.per_page) || 10;
 
     const getShowcases = async () => {
         const { data, error } = await supabase
             .from('random_showcases')
             .select()
-        res.status(200).json(data);
+            .range(perPage * (page - 1), perPage * page)
+        if (error) {
+            res.status(500).json({ error });
+            return
+        }
+        res.status(200).json({data});
     }
     getShowcases();
-
 }
