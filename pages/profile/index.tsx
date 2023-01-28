99c6a71847b0c39ce02d6cece7b42452eef33215
Flepoo <Mohamedhoshame@gmail.com>
2023-01-28 16:19:18 +0200

    fetch data to profile

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,36 +1,27 @@
 import { ReactElement, useState, useRef, useEffect } from "react";
 import { NextPage } from "next";
+import { motion } from "framer-motion";
+import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useCookies } from "react-cookie";
+import { useRouter } from "next/router";
 
 const Page: NextPage = () => {
-  const [platform, setPlatform] = useState<any>("IOS");
+  const supabase = useSupabaseClient();
+  const session = useSession();
+  const [user, setUser] = useState<any>({});
+  const handeUser = async () => {
+    const { data } = await supabase
+      .from("profiles")
+      .select("id, username, avatar_url, website, full_name")
+      .eq("id", session?.user.id);
 
-  const [timerBlur, setTimerBlur] = useState<boolean>(true);
-
-  const [countdown, setCountdown] = useState(15);
-  const timerId = useRef<any>();
-
-  useEffect(() => {
-    timerId.current = setInterval(() => {
-      setCountdown((prev) => prev - 0.5);
-    }, 1000);
-  }, []);
-
-  useEffect(() => {
-    if (countdown <= 0) {
-      clearInterval(timerId.current);
-      setTimerBlur(false);
-    }
-  }, [countdown]);
-
-  const handlePlateformUp = () => {
-    if (platform == "IOS") {
-      setPlatform("Android");
-    } else if (platform == "Android") {
-      setPlatform("Web");
-    } else if (platform == "Web") {
-      setPlatform("IOS");
-    }
+    setUser(data);
+    console.log(data);
   };
+
+  useEffect(() => {
+    handeUser();
+  }, [session]);
 
   return (
     <>
@@ -38,8 +29,12 @@ const Page: NextPage = () => {
         <div className="w-[180px] h-[180px] bg-slate-700 flex justify-center items-center rounded-2xl border-[3px] border-slate-200 shadow-md">
           image
         </div>
-        <span className="font-medium text-[40px] mt-10">Dipzin Member</span>
-        <span className="text-[20px] text-slate-400 mb-12">@dipzinmember</span>
+        <span className="font-medium text-[40px] mt-10">
+          {user[0]?.full_name}
+        </span>
+        <span className="text-[20px] text-slate-400 mb-12">
+          @{user[0]?.username}
+        </span>
 
         <div className="py-10 px-10 bg-slate-900 rounded-2xl w-[60%]">
           <span className="text-[20px]">Account Information</span>
@@ -51,21 +46,21 @@ const Page: NextPage = () => {
               <span className="text-[16px] text-slate-400">Full Name</span>
               <br />
               <div className="w-[100%] h-[65px] bg-slate-800 mt-4 rounded-xl flex items-center px-5 text-slate-100 text-[15px]">
-                Dipzin Member
+                {user[0]?.full_name}
               </div>
             </div>
             <div>
               <span className="text-[16px] text-slate-400">Username</span>
               <br />
               <div className="w-[100%] h-[65px] bg-slate-800 mt-4 rounded-xl flex items-center px-5 text-slate-100 text-[15px]">
-                @dipzinmember
+                @{user[0]?.username}
               </div>
             </div>
             <div>
               <span className="text-[16px] text-slate-400">Email Address</span>
               <br />
               <div className="w-[100%] h-[65px] bg-slate-800 mt-4 rounded-xl flex items-center px-5 text-slate-100 text-[15px]">
-                ahmedumahmoud@gmail.com
+                {session?.user.email}
               </div>
             </div>
             <div>
