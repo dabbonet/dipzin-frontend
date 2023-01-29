4f3d44f9fd9cb9cb9b8245eafeda4aa1b23eefe4
Flepoo <Mohamedhoshame@gmail.com>
2023-01-29 19:28:29 +0200

    fix profile fetch data

--- pages/api/stream/index.ts
+++ pages/api/stream/index.ts
@@ -1,32 +1,30 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import { supabase } from '../../../lib/supabase';
+import { NextApiRequest, NextApiResponse } from "next";
+import { supabase } from "../../../lib/supabase";
 
 type GetShowcasesQuery = {
-    page?: number,
-    per_page?: number
-}
+  page?: number;
+  per_page?: number;
+};
 
 export default (req: NextApiRequest, res: NextApiResponse): void => {
-    const query = req.query as GetShowcasesQuery;
-    const page = query.page || 1;
-    const perPage = query.per_page || 10;
+  const query = req.query as GetShowcasesQuery;
+  const page = query.page || 1;
+  const perPage = query.per_page || 10;
 
+  const getShowcases = async () => {
+    const from = perPage * (page - 1);
+    const to = perPage * page;
+    // console.log('from:', from, 'to: ', to);
 
+    const { data, error } = await supabase
+      .from("random_showcases")
+      .select("*")
+      .range(from + 1, to);
 
-    const getShowcases = async () => {
-        const from = (perPage * (page - 1))
-        const to = perPage * page
-        // console.log('from:', from, 'to: ', to);
-
-        const { data, error } = await supabase
-            .from('random_showcases')
-            .select('*')
-            .range(from + 1, to)
-
-        if (error) {
-            res.status(500).json({ error });
-        }
-        res.status(200).json({ data });
+    if (error) {
+      res.status(500).json({ error });
     }
-    getShowcases();
-}
+    res.status(200).json({ data });
+  };
+  getShowcases();
+};
