7a4bcae2c13a3d937c3759f8d3c449d6e42eb7db
Ahmed Mahmoud <Ahmedumahmoud@gmail.com>
2023-02-05 20:02:49 +0200

    fix build err

--- pages/home/showcase.tsx
+++ pages/home/showcase.tsx
@@ -1,5 +1,7 @@
 import { motion } from "framer-motion";
 import Link from "next/link";
+import { Key, useEffect, useState } from "react";
+import { useQuery } from "react-query";
 import Screen from "../../components/screen";
 
 type ShowcaseProps = {
@@ -66,7 +68,7 @@ const Showcase = ({ selected, setSelected }: ShowcaseProps) => {
                 className="min-w-fit p-2 h-[70px] bg-slate-900 rounded-xl flex flex-col justify-between relative border-transparent border-2 hover:border-orange-500"
                 href={{
                   pathname: "/application/[platform]/[slug]",
-                  query: { platform: getPlatform(selected.platform_id), slug: selected?.slug },
+                  query: { platform: getPlatform(selected?.platform_id), slug: selected?.slug },
                 }}
               >
                 <svg
@@ -193,7 +195,7 @@ const Showcase = ({ selected, setSelected }: ShowcaseProps) => {
           </div>
         </div>
         <div className="grid lg:grid-cols-4 xl:grid-cols-5 lg:gap-10 gap-10 grid-cols-2 ml-auto mr-auto z-50">
-          {selected.showcase.map((item: string, index: number) => (
+          {selected?.showcase.map((item: string, index: number) => (
             <Screen
               key={index}
               platform={1}
