689643aa01cf08a4c7ec1944f01c093c2a26167f
Ahmed Mahmoud <Ahmedumahmoud@gmail.com>
2023-03-27 17:04:11 +0200

    Fixes but still buggy. 🔴

--- src/app/api/stream/route.ts
+++ src/app/api/stream/route.ts
@@ -1,22 +1,35 @@
 const qs = require('qs');
 
 export async function GET(request: Request) {
-    const { searchParams } = new URL(request.url);
-    const platform = searchParams.get('platform');
-    const page = searchParams.get('page');
+    try {
+        const { searchParams } = new URL(request.url);
+        const platform = searchParams.get('platform');
+        const page = searchParams.get('page');
 
-    const params = new URLSearchParams({
-        platform: platform ?? '',
-        page: page ?? '',
-    });
+        const params = new URLSearchParams({
+            platform: platform ?? '',
+            page: page ?? '',
+        });
 
-    const req = await fetch(`https://rah.dipzin.com/api/stream?${params}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    });
-    return new Response(req.body)
+        const req = await fetch(`https://rah.dipzin.com/api/stream?${params}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (!req.ok) {
+            // Handle non-2xx HTTP response status codes
+            const errorResponse = await req.json();
+            throw new Error(errorResponse.message);
+        }
+
+        return new Response(req.body);
+    } catch (error: any) {
+        // Handle any errors that occur during the fetch request
+        console.error(error);
+        return new Response('Error: ' + error.message, { status: 500 });
+    }
 }
 
 export async function POST(request: Request) {
