f4545717ca75df421ec97c20e5d246d135b4f996
abdelrahman <abdelrahmanziedan@yahoo.com>
2023-04-19 00:20:02 +0200

    change profile and obtimize pricing and auth UI

--- src/app/(static)/access/layout.tsx
+++ src/app/(static)/access/layout.tsx
@@ -1,53 +1,50 @@
-'use client'
+"use client";
 import { useEffect, useState } from "react";
 import Image from "next/image";
 
 export default function AccessLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode
+  children: React.ReactNode;
 }) {
+  const [currentImage, setCurrentImage] = useState(0);
+  const images = [
+    "/images/hand/screen-1.png",
+    "/images/hand/screen-2.png",
+    "/images/hand/screen-3.png",
+    "/images/hand/screen-4.png",
+    "/images/hand/screen-5.png",
+  ];
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage((currentImage + 1) % images.length);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [currentImage, images.length]);
 
-    const [currentImage, setCurrentImage] = useState(0);
-    const images = [
-        "/images/hand/screen-1.png",
-        "/images/hand/screen-2.png",
-        "/images/hand/screen-3.png",
-        "/images/hand/screen-4.png",
-        "/images/hand/screen-5.png",
-    ];
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setCurrentImage((currentImage + 1) % images.length);
-        }, 1000);
-        return () => clearInterval(interval);
-    }, [currentImage, images.length]);
-
-    return (
-        <main className="flex lg:flex-row flex-col h-full lg:rounded-tl-3xl max-h-screen overflow-hidden">
-            <div className="z-10 flex-1 flex flex-col justify-center px-4">
-                {children}
-            </div>
-            <div className="relative h-[50vh] lg:h-[85vh] lg:mt-4 xl:mt-2 w-full lg:block lg:w-0 mx-auto lg:flex-1 flex-auto sm:ml-5">
-                <Image
-                    className="absolute right-0 object-contain z-0 lg:pt-24"
-                    src="/images/hand/hand.png"
-                    fill
-                    alt=""
-                    sizes="100%"
-                    unoptimized
-                />
-                {images[currentImage] && (
-                    <Image
-                        className="absolute inset-0 object-contain h-full lg:pt-24"
-                        src={images[currentImage]}
-                        fill
-                        alt=""
-                        sizes="100%"
-                        unoptimized
-                    />
-                )}
-            </div>
-        </main>
-    )
+  return (
+    <main className="flex flex-wrap h-full max-h-screen">
+      <div className="z-10 flex-1 flex flex-col justify-center px-4">
+        {children}
+      </div>    
+      <div className="relative h-[50vh] lg:h-[85vh] xl:mt-2 w-full mx-auto lg:flex-1 flex justify-center items-center">
+        <img
+          className=" w-[50%] h-[70%] -translate-y-4 -translate-x-24 z-50"
+          src="/images/assets/phoneAuth.svg"
+          alt=""
+          
+        />
+        {images[currentImage] && (
+          <Image
+            className=" object-contain !w-[50%] !h-[70%]"
+            src={images[currentImage]}
+            fill
+            alt=""
+            sizes=""
+            unoptimized
+          />
+        )}
+      </div>
+    </main>
+  );
 }
