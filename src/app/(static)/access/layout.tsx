7799f275489671891f59486655826a2b44f3a02c
abdelrahman <abdelrahmanziedan@yahoo.com>
2023-04-19 18:51:55 +0200

    auth UI

--- src/app/(static)/access/layout.tsx
+++ src/app/(static)/access/layout.tsx
@@ -9,11 +9,11 @@ export default function AccessLayout({
 }) {
   const [currentImage, setCurrentImage] = useState(0);
   const images = [
-    "/images/hand/screen-1.png",
-    "/images/hand/screen-2.png",
-    "/images/hand/screen-3.png",
-    "/images/hand/screen-4.png",
-    "/images/hand/screen-5.png",
+    "/images/hand/Screenshot-1.png",
+    "/images/hand/Screenshot-2.png",
+    "/images/hand/Screenshot-3.png",
+    "/images/hand/Screenshot-4.png",
+    "/images/hand/Screenshot-5.png",
   ];
   useEffect(() => {
     const interval = setInterval(() => {
@@ -23,20 +23,20 @@ export default function AccessLayout({
   }, [currentImage, images.length]);
 
   return (
-    <main className="flex flex-wrap h-full max-h-screen">
+    <main className="flex flex-wrap h-full max-h-screen gap-y-10">
       <div className="z-10 flex-1 flex flex-col justify-center px-4">
         {children}
       </div>    
-      <div className="relative h-[50vh] lg:h-[85vh] xl:mt-2 w-full mx-auto lg:flex-1 flex justify-center items-center">
+      <div className="relative h-[50vh] lg:h-[85vh] xl:mt-2 w-full mx-auto lg:flex-1 overflow-hidden">
         <img
-          className=" w-[50%] h-[70%] -translate-y-4 -translate-x-24 z-50"
+          className=" max-w-[100%] z-50 mx-auto max-h-[100%]"
           src="/images/assets/phoneAuth.svg"
           alt=""
           
         />
         {images[currentImage] && (
           <Image
-            className=" object-contain !w-[50%] !h-[70%]"
+            className=" object-contain -z-50"
             src={images[currentImage]}
             fill
             alt=""
