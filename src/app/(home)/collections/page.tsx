711d24b1f6db76f31dd00b2894f6e2bbd1e53150
abdelrahman <abdelrahmanziedan@yahoo.com>
2023-05-07 18:25:40 +0200

    full collection ui

--- src/app/(home)/collections/page.tsx
+++ src/app/(home)/collections/page.tsx
@@ -3,46 +3,39 @@ import CollectionCard from "@/components/ui/CollectionCard";
 import { FC, useEffect, useState } from "react";
 
 const Collections: FC = () => {
-  const [data, setData] = useState(null);
-  useEffect(() => {
-    async function getCollection() {
-      try {
-        const response = await fetch("api/collections", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            token: localStorage.getItem("token"),
-          }),
-        });
-        const data = await response.json();
-        setData(data);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    getCollection();
-  }, []);
+  // const [data, setData] = useState(null);
+
+  // useEffect(() => {
+  //   async function getCollection() {
+  //     try {
+  //       const response = await fetch("https://rah.dipzin.com/api/collections", {
+  //         method: "POST",
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //           authorization: `Bearer ${localStorage.getItem("token")}`,
+  //         },
+  //       });
+  //       const data = await response.json();
+  //       setData(data);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   }
+  //   getCollection();
+  // }, []);
 
   return (
     <div className=" mt-4">
+      <div className=" w-fit bg-slate-800 flex p-2 items-center rounded-3xl ml-auto mb-4">
+        <div className=" py-1 px-3">Personal</div>
+        <div className=" py-1 px-3 bg-slate-700 rounded-3xl">Community</div>
+      </div>
       <div className=" grid lg:grid-cols-3 gap-8 md:grid-cols-2 grid-cols-1">
-        {data ? (
-          data.map((collection) => {
-            return (
-              <CollectionCard
-                key={collection.id}
-                name={collection.name}
-                description={collection.description}
-              />
-            );
-          })
-        ) : (
-          <div className=" bg-slate-800 p-4 border border-slate-700 border-solid rounded-3xl">
-            new collection card
-          </div>
-        )}
+        <CollectionCard name='colllection name' description='1m' />
+        <CollectionCard name='colllection name' description='1m' />
+        <CollectionCard name='colllection name' description='1m' />
+        <CollectionCard name='colllection name' description='1m' />
+        <CollectionCard name='colllection name' description='1m' />
       </div>
     </div>
   );
--- src/components/ui/CollectionCard.tsx
+++ src/components/ui/CollectionCard.tsx
@@ -1,10 +1,25 @@
+"use client";
 import React from "react";
 
 const CollectionCard = ({ name, description }) => {
   return (
-    <div className=" bg-slate-800 p-4 border-solid border border-slate-700">
-      <h2 className="text-2xl font-bold">{name}</h2>
-      <p className="text-gray-600">{description}</p>
+    <div className=" bg-slate-800 p-4 border-solid border border-slate-700 rounded-3xl">
+      <div className=" grid grid-cols-3 gap-3">
+        <img src="/images/assets/collection-screen2.svg" alt="" className="" />
+        <img src="/images/assets/collection-screen1.svg" alt="" className="" />
+        <div className=" flex flex-col gap-3 items-center">
+          <img src="/images/assets/collection-app1.svg" alt="" className="" />
+          <img src="/images/assets/collection-app2.svg" alt="" className="" />
+          <img src="/images/assets/collection-app3.svg" alt="" className="" />
+        </div>
+      </div>
+      <div className=" mt-4 flex justify-between items-center">
+        <div>
+          <h2 className="text-2xl font-bold">{name}</h2>
+          <p className="text-gray-600">Modified:{description}</p>
+        </div>
+        <div className=" bg-slate-300 text-slate-800 rounded-md px-2 py-3">ooo</div>
+      </div>
     </div>
   );
 };
