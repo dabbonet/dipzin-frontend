dd21124f995ae96b716a9d29d037ce1177268e02
abdelrahman <abdelrahmanziedan@yahoo.com>
2023-05-09 21:01:45 +0200

    change ImageLoader URL & Handle Erorr on app data

--- src/app/(static)/app/[platform]/[slug]/page.tsx
+++ src/app/(static)/app/[platform]/[slug]/page.tsx
@@ -13,10 +13,12 @@ export default async function AppPage({params: { slug, platform }}: {params: app
     let apps = await getApps({ slug });
 
     // Filter apps to get the selected app
-    const app = apps.data.filter(data => data.attributes.platform.data.attributes.name.toLowerCase() === platform.toLowerCase())[0].attributes;
+    const app = apps.data.filter(data => data.attributes.platform.data.attributes.name.toLowerCase() === platform.toLowerCase())[0]?.attributes;
 
-    if (!apps) {
-        notFound();
+    
+
+    if (!apps || !app) {
+        return <h1 className=' mx-auto mt-[35%] w-fit h-fit'>app not found</h1>
     }
     return (
         <>
