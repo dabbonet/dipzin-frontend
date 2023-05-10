7561abf0e995107650e9a84df861ff2672517c11
abdelrahman <abdelrahmanziedan@yahoo.com>
2023-05-10 19:08:34 +0200

    collection ui and ui in single collection

--- src/lib/imageLoader.ts
+++ src/lib/imageLoader.ts
@@ -3,5 +3,5 @@ const PREFIXES = ['', 'thumbnail_', 'small_', 'medium_', 'large_'];
 export default function imageLoader({ src, quality }) {
     const prefixIndex = Math.floor((quality || 0) / 10);
     const prefix = PREFIXES[prefixIndex] || '';
-    return `https://dipzinapplications.s3.us-west-1.amazonaws.com/${prefix}${src}`
+    return `https://dipzin.s3.us-east-1.amazonaws.com/${prefix}${src}`
 }
