b99088b7ddccbf72fff574d7fd76133012b3485e
abdelrahman <abdelrahmanziedan@yahoo.com>
2023-05-14 22:18:46 +0200

    using descriptive name

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,20 +1,15 @@
 'use client'
 import Stream from "@/components/Stream";
-import { Component } from "@/components/accessAndUbgrade";
-import { useDialog } from "@/context/useDialog";
-import { useEffect } from "react";
+import { AccessOrUpgradeCard } from "@/components/accessAndUbgrade";
 import { Toaster } from "react-hot-toast"
 
 export default function Home() {
-  const {setTimes} = useDialog()
-  useEffect(() => {
-    setTimes(10)
-  })
+  
   return (
     <>
       <Stream />
       <Toaster position="top-center" />
-      <Component />
+      <AccessOrUpgradeCard />
     </>
   );
 }
