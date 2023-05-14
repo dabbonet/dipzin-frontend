9d7d3e977a3224b3d100f4fde32a76eceb0e21c8
abdelrahman <abdelrahmanziedan@yahoo.com>
2023-05-14 16:16:39 +0200

    access dialog and upgrade dialog based on token

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,15 +1,20 @@
 'use client'
 import Stream from "@/components/Stream";
-import { UpgradeMemberCard } from "@/components/UpgradeMemberCard";
+import { Component } from "@/components/accessAndUbgrade";
+import { useDialog } from "@/context/useDialog";
+import { useEffect } from "react";
 import { Toaster } from "react-hot-toast"
 
 export default function Home() {
-
+  const {setTimes} = useDialog()
+  useEffect(() => {
+    setTimes(10)
+  })
   return (
     <>
       <Stream />
       <Toaster position="top-center" />
-      <UpgradeMemberCard />
+      <Component />
     </>
   );
 }
--- src/components/accessAndUbgrade.tsx
+++ src/components/accessAndUbgrade.tsx
@@ -3,12 +3,109 @@ import Icons from "./Icons";
 import { cn } from "@/lib/utils";
 import { useDialog } from "@/context/useDialog";
 import SparkleButton from "@/ui/SparkleButton";
+import AccessComponent from "./AccessComponent";
+import { toast } from "react-hot-toast";
+import { setToken, SignIn, verifyOtp } from "@/lib/auth";
+import OtpAccessComponent from "./OtpAccessComponent";
+import { useRouter } from "next/navigation";
 
 function formatTime(seconds: number): string {
-  // const mins = Math.floor(seconds / 60).toString().padStart(2, '0');
   const secs = (seconds % 60).toString().padStart(2, '0');
   return `${secs}s`;
 }
+
+
+export const Component = () => {
+  if (localStorage.getItem('token')) { 
+    return  <UpgradeMemberCard/>
+  }
+  return <div className=" fixed w-full h-full inset-0 bg-opacity-20 bg-gradient-to-tr from-[#0D1018] to-[] backdrop-blur-[30px]  flex justify-center items-center z-50">
+    <div className=" w-fit h-fit bg-slate-900 bg-opacity-60 rounded-2xl px-16 py-20">
+      <AccessCard/>
+    </div>
+  </div>
+}
+
+
+const AccessCard = () => {
+  // global variable for email
+  const [email, setEmail] = useState("");
+  // about otp card
+  const [showOtpCard, setShowOtpCard] = useState(false)
+  const [otp, setOtp] = useState<number>();
+  const [failedMessage, setFailedMessage] = useState(false);
+  const [disabelButton, setDisabelButton] = useState(false);
+  const router = useRouter();
+  const handleResend = async () => {
+    SignIn(email);
+    setFailedMessage(false);
+  };
+  const handleClick = async () => {
+    setDisabelButton(true);
+    setFailedMessage(false);
+    const data = await verifyOtp(email, otp);
+    const { token } = await data.json();
+    if (token) {
+      setToken(token);
+      if (data.status === 200) {
+        router.push("/");
+      } else {
+        router.push("/account");
+      }
+    } else {
+      toast.error("invalid code , you can resend after 30 seconds", {
+        style: {
+          backgroundColor: "orange",
+          color: "white",
+        },
+      });
+      setTimeout(() => {
+        setFailedMessage(true);
+        setDisabelButton(false);
+      }, 30000);
+    }
+  };
+  // about access
+  const [disableProcess, setDisableProcess] = useState(false);
+  const handleChange = (event: any) => {
+    setEmail(event.target.value);
+  };
+  const handleSubmit = async () => {
+    setDisableProcess(true);
+    const regextMatchEmail =
+      /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/;
+
+    if (!email.match(regextMatchEmail)) {
+      setTimeout(() => {
+        setDisableProcess(false);
+      }, 2000);
+      return toast.error("please enter a valid email", {
+        style: {
+          backgroundColor: "orange",
+          color: "white",
+        },
+        duration: 2000,
+      });
+    }
+
+    const res = await SignIn(email);
+    if (res) {
+      setShowOtpCard(true);
+    } else {
+      setDisableProcess(false);
+      return toast.error("Something Went wrong");
+    }
+  };
+  if (showOtpCard) return <OtpAccessComponent email={email} setOtp={setOtp} failedMessage={failedMessage} handleClick={handleClick} handleResend={handleResend} disabelButton={disabelButton} />
+  return <AccessComponent handleChange={handleChange} disableProcess={disableProcess} handleSubmit={handleSubmit} />
+}
+
+
+
+
+
+
+
 
 const UpgradeMemberCard = () => {
   const [show, setShow] = useState<boolean>(false)
@@ -90,4 +187,3 @@ const UpgradeMemberCard = () => {
   )
 };
 
-export { UpgradeMemberCard };
