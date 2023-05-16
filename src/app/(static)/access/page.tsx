8d1d3c2baa632dd04635ee79f281868d30f18e3c
abdelrahman <abdelrahmanziedan@yahoo.com>
2023-05-16 19:24:22 +0200

    refactor access component and otp access component

--- src/app/(static)/access/page.tsx
+++ src/app/(static)/access/page.tsx
@@ -1,47 +1,11 @@
 "use client";
 import AccessComponent from "@/components/AccessComponent";
+import { FC } from "react";
 
-import { SignIn } from "@/lib/auth";
-import { useRouter } from "next/navigation";
-import { FC, useState } from "react";
-import { toast } from "react-hot-toast";
 
 const Access: FC = () => {
-  const router = useRouter();
-  const [email, setEmail] = useState("");
-  const [disableProcess, setDisableProcess] = useState(false);
-  const handleChange = (event: any) => {
-    setEmail(event.target.value);
-  };
 
-  const handleSubmit = async () => {
-    setDisableProcess(true);
-    const regextMatchEmail =
-      /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/;
-
-    if (!email.match(regextMatchEmail)) {
-      setTimeout(() => {
-        setDisableProcess(false);
-      }, 2000);
-      return toast.error("please enter a valid email", {
-        style: {
-          backgroundColor: "orange",
-          color: "white",
-        },
-        duration: 2000,
-      });
-    }
-
-    const res = await SignIn(email);
-    if (res) {
-      router.push(`/access/otp?email=${email}`);
-    } else {
-      setDisableProcess(false);
-      return toast.error("Something Went wrong");
-    }
-  };
-
-  return <AccessComponent handleChange={handleChange} handleSubmit={handleSubmit} disableProcess={disableProcess} />;
+  return <AccessComponent  />;
 };
 
 export default Access;
