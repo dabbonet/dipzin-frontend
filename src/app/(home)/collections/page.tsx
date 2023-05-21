a3a5f1acd6ee2e5660b3f54927ca4d2ab3ce03ba
abdelrahman <abdelrahmanziedan@yahoo.com>
2023-05-22 01:04:35 +0200

    fix the collection UI and solve the PR comments

--- src/app/(home)/collections/page.tsx
+++ src/app/(home)/collections/page.tsx
@@ -1,28 +1,8 @@
 "use client";
 import CollectionCard from "@/components/ui/CollectionCard";
-import { FC, useEffect, useState } from "react";
+import { FC } from "react";
 
 const Collections: FC = () => {
-  // const [data, setData] = useState(null);
-
-  // useEffect(() => {
-  //   async function getCollection() {
-  //     try {
-  //       const response = await fetch("https://rah.dipzin.com/api/collections", {
-  //         method: "POST",
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //           authorization: `Bearer ${localStorage.getItem("token")}`,
-  //         },
-  //       });
-  //       const data = await response.json();
-  //       setData(data);
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   }
-  //   getCollection();
-  // }, []);
 
   return (
     <div className=" mt-4">
