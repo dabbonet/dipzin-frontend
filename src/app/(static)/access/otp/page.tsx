73277de1709d9ec351ae7e59ca6d72ea0cf55159
abdelrahman <abdelrahmanziedan@yahoo.com>
2023-05-24 00:30:41 +0200

    clean the code

--- src/app/(static)/access/otp/page.tsx
+++ src/app/(static)/access/otp/page.tsx
@@ -1,29 +1,8 @@
 "use client";
-import { SignIn, setToken, verifyOtp } from "@/lib/auth";
-import { useSearchParams, useRouter } from "next/navigation";
-import {  useEffect, useState , FC} from "react";
 import OtpAccessComponent from "@/components/OtpAccessComponent";
-import { toast } from "react-hot-toast";
 
-const Otp: FC = () => {
 
-  const otpStringLength = 6
-  const searchParams = useSearchParams();
-    const router = useRouter();
-    const email = searchParams.get("email");
-    const [otp, setOtp] = useState<number>();
-    const [failedMessage, setFailedMessage] = useState(false);
-    const [disabelButton, setDisabelButton] = useState(true);
-    useEffect(() => {
-      const otpAsStrnig = `${otp}`
-      if (otpAsStrnig.length === otpStringLength ) {
-        setDisabelButton(false);
-      } else {
-        
-        setDisabelButton(true);
-      }
-    }, [otp])
-
+const Otp = () => {
   
   return <OtpAccessComponent />
 
