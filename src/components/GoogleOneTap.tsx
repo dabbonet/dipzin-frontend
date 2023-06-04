aefd95ec26fc62710cac4c9786e151133cb65d1e
abdelrahman <abdelrahmanziedan@yahoo.com>
2023-06-04 17:35:52 +0300

    connect my data to back-end

--- src/components/GoogleOneTap.tsx
+++ src/components/GoogleOneTap.tsx
@@ -1,18 +1,61 @@
 'use client'
-import React from 'react'
-import { useGoogleOneTapLogin } from 'react-google-one-tap-login'
+import { getUser, setToken } from '@/lib/auth'
+import React, { useEffect, useState } from 'react'
+import GoogleOneTapLogin  from 'react-google-one-tap-login'
+import { toast } from 'react-hot-toast'
 
 const GoogleOneTap = ({ children }) => {
-    useGoogleOneTapLogin({
-        googleAccountConfigs:{
-          client_id: '240831230179-e7t797p09bh0bdhhk9gm5bt45cdddmt2.apps.googleusercontent.com'
+  const [isUserAuth, setIsUserAuth] = useState(false)
+  useEffect(() => {
+    async function isUserAuthintcated() {
+      const isAuthented = await getUser()
+      if (isAuthented) {
+        setIsUserAuth(true)
+      }
+    }
+    isUserAuthintcated()
+  }, [])
+  
+  
+  const handleSuccess =  async (res) => {
+    const cookies = document.cookie.split(";").map(x => {
+      const [name, value] = x.trim().split("=");
+      return { name, value };
+    });
+    const referralToken = cookies?.filter(x => x.name == 'referral-token')[0]?.value ?? null;
+    try {
+      const req = await fetch('/api/user/google-one-tap', {
+        method: 'post',
+        headers: {
+          'Content-Type':'application/json'
         },
-        onSuccess: (res)=> console.log(res)
+        body: JSON.stringify({
+          data: {
+            email: res.email,
+            name: res.name,
+            referralCode: referralToken
+          }
+        })
       })
+      const response = await req.json()
+      if (req.ok) {
+        setToken(response.token)
+      }
+    } catch (error) {
+      toast.remove()
+      toast.error('somthing went wrong')
+    }
+    }
+      
+    if (isUserAuth) {
+      return <>
+        {children}
+      </>
+  }
   return (
-      <>
+      <GoogleOneTapLogin googleAccountConfigs={{client_id: process.env.GOOGLE_CLIENT_ID}} onSuccess={handleSuccess}>
           {children}
-    </>
+    </GoogleOneTapLogin>
   )
 }
 
