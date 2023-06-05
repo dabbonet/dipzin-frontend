9177c5fbe0a8d3c7ae1e9ebcf805aeca6a8bf6d3
Ahmed Mahmoud <Ahmedumahmoud@gmail.com>
2023-06-05 02:40:14 +0200

    Moved root layout to be only one, ui small updates

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,6 @@
 'use client'
+import Banner from "@/components/Banner";
+import HomeNavigator from "@/components/HomeNavigator";
 import Stream from "@/components/Stream";
 import { Toaster } from "react-hot-toast"
 
@@ -6,6 +8,8 @@ import { Toaster } from "react-hot-toast"
 export default function Home() {
   return (
     <>
+      <Banner />
+      <HomeNavigator />
       <Stream />
       <Toaster position="top-center" />
     </>
