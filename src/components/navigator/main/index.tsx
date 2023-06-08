a616ee2ac273341d960234b9f5a81f3509b88381
abdelrahman <abdelrahmanziedan@yahoo.com>
2023-06-08 17:35:19 +0300

    remove navbar from access page

--- src/components/navigator/main/index.tsx
+++ src/components/navigator/main/index.tsx
@@ -5,13 +5,18 @@ import { useRouter } from "next/router";
 import GuestNavigator from "./guest";
 import MainNavigator from "./main";
 import { useAuth } from "@/lib/auth";
+import { usePathname } from "next/navigation";
+
+const notAllowShowNavigatioPaths = ['/access', '/access/otp' , '/profile/step-1','/profile/step-2','/profile/step-3','/account']
 
 const Navigator = ({ type }: any) => {
+  const pathName = usePathname()
   const { user, loading } = useAuth();
   useEffect(() => {
     console.log("user:", user)
   }, [user])
-
+  
+  if(notAllowShowNavigatioPaths.includes(pathName)) return
   return (
     <motion.div
       // initial={{ y: 200 }}
