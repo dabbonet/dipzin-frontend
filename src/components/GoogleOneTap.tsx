429ade06b293cce1e638541d9daaf1fe7fce0b99
abdelrahman <abdelrahmanziedan@yahoo.com>
2023-06-12 15:13:40 +0300

    fix google one tap branch layout

--- src/components/GoogleOneTap.tsx
+++ src/components/GoogleOneTap.tsx
@@ -2,13 +2,16 @@
 import {  setToken, useAuth } from '@/lib/auth'
 import { invetaionAndReferralTokens } from '@/lib/tokens'
 import React from 'react'
-import GoogleOneTapLogin  from 'react-google-one-tap-login'
+import GoogleOneTapLogin from '@rheasmith/react-google-one-tap-login';
 import { toast } from 'react-hot-toast'
+import { usePathname } from 'next/navigation';
 
+
+const notShowInPathArr = ['/access' , '/access/otp']
 
 const GoogleOneTap = ({ children }) => {
   const {user} = useAuth()
-  
+  const path = usePathname()
   
   const handleSuccess =  async (res) => {
     const {referralToken , invitationToken} = invetaionAndReferralTokens()
@@ -30,6 +33,7 @@ const GoogleOneTap = ({ children }) => {
       const response = await req.json()
       if (req.ok) {
         setToken(response.token)
+        window.location.reload()
       }
     } catch (error) {
       toast.remove()
@@ -37,7 +41,7 @@ const GoogleOneTap = ({ children }) => {
     }
     }
       
-    if (user) {
+    if (user || notShowInPathArr.includes(path)) {
       return <>
         {children}
       </>
