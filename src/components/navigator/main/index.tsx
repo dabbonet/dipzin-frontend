9ca6b2fd081b8dc7b28d2f74f37a569eb2d9252a
Ahmed Mahmoud <Ahmedumahmoud@gmail.com>
2023-06-18 20:44:54 +0300

    add hiding

--- src/components/navigator/main/index.tsx
+++ src/components/navigator/main/index.tsx
@@ -1,13 +1,22 @@
 'use client'
 import { motion } from "framer-motion";
 import { useState, useEffect } from "react";
-import { useRouter } from "next/router";
 import GuestNavigator from "./guest";
 import MainNavigator from "./main";
 import { useAuth } from "@/lib/auth";
+import { usePathname } from "next/navigation";
+
+const notAllowShowNavigatioPaths = ['/access', '/access/otp' , '/profile/step-1','/profile/step-2','/profile/step-3','/account']
 
 const Navigator = ({ type }: any) => {
+  const pathName = usePathname()
   const { user, loading } = useAuth();
+  useEffect(() => {
+    console.log("user:", user)
+  }, [user])
+
+
+  if(notAllowShowNavigatioPaths.includes(pathName)) return
   return (
     <motion.div
       // initial={{ y: 200 }}
@@ -18,13 +27,10 @@ const Navigator = ({ type }: any) => {
       {!loading && user &&
         <MainNavigator type={type} />
       }
-
       {!loading && !user &&
         <GuestNavigator />
       }
-
     </motion.div>
   );
 };
-
 export default Navigator;
