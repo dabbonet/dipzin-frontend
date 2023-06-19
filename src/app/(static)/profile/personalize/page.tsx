8478508ad59c8d497e3bf089f9bc58caff162079
Ahmed Mahmoud <Ahmedumahmoud@gmail.com>
2023-06-19 23:32:30 +0200

    updates

--- src/app/(static)/profile/personalize/page.tsx
+++ src/app/(static)/profile/personalize/page.tsx
@@ -3,7 +3,7 @@
 import { getToken } from "@/lib/auth";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import {  useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { toast } from "react-hot-toast";
 
 export default function page({ }) {
@@ -18,15 +18,9 @@ export default function page({ }) {
             const [positionsReq, interestsReq] = await Promise.all([
                 fetch('https://rah.dipzin.com/api/positions', {
                     method: 'GET',
-                    headers: {
-                        Authorization: `Bearer ${getToken()}`
-                    }
                 }),
                 fetch('https://rah.dipzin.com/api/interests', {
                     method: 'GET',
-                    headers: {
-                        Authorization: `Bearer ${getToken()}`
-                    }
                 })
             ]);
             const [positionsRes, interestsRes] = await Promise.all([
@@ -48,48 +42,43 @@ export default function page({ }) {
             }
             console.log(userPositions)
         }
-        return <label  htmlFor={id} className=" flex items-center bg-slate-900 py-2 pl-4 pr-7 rounded-lg">
-            <input onClick={clickLabel} type="checkbox" name={name} id={id} className=" mr-4  before:checked:content-['✓'] before:checked:bg-aqua-600 bg-opacity-0 before:rounded  before:w-5 relative before:absolute  before:h-5 before:bg-slate-800 before:-top-1 before:-left-1 before:flex before:items-center before:justify-center"/>
-        <div className=" flex justify-center items-center bg-slate-800 p-2 border border-solid border-slate-700 rounded-lg mr-4 checked:border-aqua-300">
-            <img src="/images/assets/product-designer.svg" alt="" />
-        </div>
+        return <label htmlFor={id} className=" flex items-center bg-slate-900 py-2 pl-4 pr-7 rounded-lg">
+            <input onClick={clickLabel} type="checkbox" name={name} id={id} className=" mr-4  before:checked:content-['✓'] before:checked:bg-aqua-600 bg-opacity-0 before:rounded  before:w-5 relative before:absolute  before:h-5 before:bg-slate-800 before:-top-1 before:-left-1 before:flex before:items-center before:justify-center" />
+            <div className=" flex justify-center items-center bg-slate-800 p-2 border border-solid border-slate-700 rounded-lg mr-4 checked:border-aqua-300">
+                <img src="/images/assets/product-designer.svg" alt="" />
+            </div>
             <span className=" w-fit text-xs font-normal">{name}</span>
-    </label>
+        </label>
     }
-    const InterestsComponent = ({tilte , id}) => {
+    const InterestsComponent = ({ tilte, id }) => {
 
         const clickButton = (e) => {
-            if (e.target.style.borderColor === '') {
-                e.target.style.borderColor = '#00dbae'
-            } else {
-                e.target.style.borderColor = ''
-            }
             if (userInterests.includes(id)) {
                 userInterests = userInterests.filter(el => el !== id)
             } else {
-                userInterests = [...userInterests , id]
+                userInterests = [...userInterests, id]
             }
         }
-      return (
-          <button
-              className={` text-slate-200 py-2 px-4 rounded-2xl bg-slate-900 border-solid border `}
-              onClick={clickButton}
-          >
-              {tilte}
-          </button>
-      )
+        return (
+            <button
+                className={` text-slate-200 py-2 px-4 rounded-2xl bg-slate-900 border border-transparent`}
+                onClick={clickButton}
+            >
+                {tilte}
+            </button>
+        )
     }
     const FullPositionComponent = () => {
         return <div className=" grid grid-cols-1 lg:grid-cols-2 mt-3 gap-4">
             {positions?.map(el => (
-                <PositionComponent id={el.id} name={el.attributes.name}/>
+                <PositionComponent id={el.id} name={el.attributes.name} />
             ))}
         </div>
     }
     const FullInterestsComponent = () => {
         return <div className=" flex flex-wrap gap-3 mb-8">
-                {interests?.map(el => (
-                <InterestsComponent id={el.id} tilte={el.attributes.name}/>
+            {interests?.map(el => (
+                <InterestsComponent id={el.id} tilte={el.attributes.name} />
             ))}
         </div>
     }
@@ -99,7 +88,7 @@ export default function page({ }) {
                 method: 'POST',
                 headers: {
                     Authorization: `Bearer ${getToken()}`,
-                    "Content-Type" : 'application/json'
+                    "Content-Type": 'application/json'
                 },
                 body: JSON.stringify({
                     data: {
@@ -111,7 +100,7 @@ export default function page({ }) {
                 method: 'POST',
                 headers: {
                     Authorization: `Bearer ${getToken()}`,
-                    "Content-Type" : 'application/json'
+                    "Content-Type": 'application/json'
                 },
                 body: JSON.stringify({
                     data: {
@@ -121,12 +110,12 @@ export default function page({ }) {
             })
         ]);
         if (positionsReq.ok && interestsReq.ok) {
-            router.push('/profile/step-3')
+            router.push('/profile/enjoy')
         } else {
             toast.remove()
             toast.error('something went wrong')
         }
-       
+
     }
     return <div className=" flex gap-x-36 flex-wrap justify-center items-center">
         <div className=" flex-1">
@@ -147,11 +136,11 @@ export default function page({ }) {
         </div>
         <div className=" flex-1">
             <p className=" text-slate-300">Which best describes you?</p>
-            <FullPositionComponent/>
+            <FullPositionComponent />
             <div className=" mt-9">
                 <h3 className=" text-slate-300 mb-1 text-base font-normal">Interests</h3>
                 <p className=" text-slate-500 mb-4">Help us develop and prioritize features, and customize your experience.</p>
-                <FullInterestsComponent/>
+                <FullInterestsComponent />
                 <div className=" flex justify-between items-center">
                     <Link href='/' className=" text-slate-400">Skip</Link>
                     <div className=" flex items-center gap-4">
