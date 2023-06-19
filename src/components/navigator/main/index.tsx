a2ad1be0a586548ed7c222ca2cf02024d03e9428
Ahmed Mahmoud <Ahmedumahmoud@gmail.com>
2023-06-19 18:44:55 +0200

    add new search UI and functionality

--- src/components/navigator/main/index.tsx
+++ src/components/navigator/main/index.tsx
@@ -4,25 +4,18 @@ import { useState, useEffect } from "react";
 import GuestNavigator from "./guest";
 import MainNavigator from "./main";
 import { useAuth } from "@/lib/auth";
-import { usePathname } from "next/navigation";
 
-const notAllowShowNavigatioPaths = ['/access', '/access/otp' , '/profile/step-1','/profile/step-2','/profile/step-3','/account']
 
 const Navigator = ({ type }: any) => {
-  const pathName = usePathname()
   const { user, loading } = useAuth();
-  useEffect(() => {
-    console.log("user:", user)
-  }, [user])
 
 
-  if(notAllowShowNavigatioPaths.includes(pathName)) return
   return (
     <motion.div
       // initial={{ y: 200 }}
       // animate={{ y: 0 }}
       // transition={{ type: "spring", stiffness: 100 }}
-      className="fixed w-full left-0 bottom-0 flex justify-center z-40"
+      className="fixed w-fit left-0 bottom-0 flex justify-center z-40"
     >
       {!loading && user &&
         <MainNavigator type={type} />
