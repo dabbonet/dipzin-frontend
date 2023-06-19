1b48dca2c75c831d214a8f266bb39c1dd59f0cc4
Ahmed Mahmoud <Ahmedumahmoud@gmail.com>
2023-06-19 16:53:02 +0200

    make UI to react comps

--- src/components/navigator/main/menu.tsx
+++ src/components/navigator/main/menu.tsx
@@ -14,68 +14,34 @@ const Menu = () => {
         // transition={{ type: "spring", duration: 0.6, delay: 0.3 }}
         >
             <div className='flex space-x-4 h-[90%] p-2 px-4 min-w-[1000px] flex-col'>
-                <div className=' flex gap-x-2 mb-6 ml-4'>
-                    <p className=' text-slate-300 text-[10px]'>Privacy</p>
-                    <p className=' text-slate-300 text-[10px]'>Terms of Service</p>
-                    <p className=' text-slate-300 text-[10px]'>Copyrights</p>
+                <div className=' flex gap-x-2 mb-3 ml-4 text-xs'>
+                    <p className=' text-slate-300 '>Privacy</p>
+                    <p className=' text-slate-300 '>Terms of Service</p>
+                    <p className=' text-slate-300 '>Copyrights</p>
                 </div>
-                <div className=' flex gap-x-4'>
+                <div className=' flex gap-x-4 mb-2'>
                     <div className=' flex gap-x-8'>
                         <div className=' flex flex-col gap-y-2'>
                             <p className=' text-slate-400'>Resources</p>
-                            <div className=' flex gap-x-2'>
-                                <img className=" w-4" src="/images/assets/refresh.svg" alt="refresh" />
-                                <div>
-                                    <Link  href='/' className='text-base hover:text-aqua-500'>Stream</Link>
-                                    <p className=' text-slate-400 text-[10px]'>The main gate to discover dipzin platform.</p>
-                                </div>
-                            </div>
-                            <div className=' flex gap-x-2'>
-                                <img className=" w-4" src="/images/assets/folder-2.svg" alt="refresh" />
-                                <div>
-                                    <Link  href='/collections' className='  text-base hover:text-aqua-500'>Community Collections</Link>
-                                    <p className=' text-slate-400 text-[10px]'>The main gate to discover dipzin platform.</p>
-                                </div>
-                            </div>
-                            <div className=' flex gap-x-2'>
-                                <img className=" w-4" src="/images/assets/folder-2.svg" alt="refresh" />
-                                <div>
-                                    <Link  href='/tools' className='  text-base hover:text-aqua-500'>Tools</Link>
-                                    <p className=' text-slate-400 text-[10px]'>The main gate to discover dipzin platform.</p>
-                                </div>
-                            </div>
+
+                            <Resources />
+                            <Resources />
+                            <Resources />
+
                         </div>
                         <div className=' flex flex-col gap-y-2'>
                             <p className=' text-slate-400'>Company</p>
-                            <div className=' flex gap-x-2'>
-                                <img className=" w-4" src="/images/assets/refresh.svg" alt="refresh" />
-                                <div>
-                                    <Link  href='/blog' className=' text-base hover:text-aqua-500'>Blog</Link>
-                                    <p className=' text-slate-400 text-[10px]'>The main gate to discover dipzin platform.</p>
-                                </div>
-                            </div>
-                            <div className=' flex gap-x-2'>
-                                <img className=" w-4" src="/images/assets/folder-2.svg" alt="refresh" />
-                                <div>
-                                    <Link  href='/about' className='  text-base hover:text-aqua-500'>About</Link>
-                                    <p className=' text-slate-400 text-[10px]'>The main gate to discover dipzin platform.</p>
-                                </div>
-                            </div>
-                            <div className=' flex gap-x-2'>
-                                <img className=" w-4" src="/images/assets/folder-2.svg" alt="refresh" />
-                                <div>
-                                    <Link href='/contact-us'  className=' text-base hover:text-aqua-500'>Contact Us</Link>
-                                    <p className=' text-slate-400 text-[10px]'>The main gate to discover dipzin platform.</p>
-                                </div>
-                            </div>
+                            <Resources />
+                            <Resources />
+                            <Resources />
                         </div>
                     </div>
-                    <div className=' flex flex-col gap-3'>
+                    {/* <div className=' flex flex-col gap-3'>
                         <p className=' text-slate-400'>Feature Release</p>
                         <div className=' overflow-hidden'>
                             <img src="/images/assets/video.svg" className=' w-[266px] h-[148px] -ml-11' alt="" />
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </motion.div>
@@ -83,6 +49,20 @@ const Menu = () => {
 }
 
 export default Menu
+
+
+const Resources = () => {
+
+    return (
+        <div className=' flex items-start gap-x-2'>
+            <img className="mt-2 w-4 text-white" src="/images/assets/refresh.svg" alt="refresh" />
+            <div>
+                <Link href='/' className='text-base hover:text-aqua-500'>Stream</Link>
+                <p className=' text-slate-400 text-[10px]'>The main gate to discover dipzin <br /> platform.</p>
+            </div>
+        </div>
+    )
+}
 
 
 
