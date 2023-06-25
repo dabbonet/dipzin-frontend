2c17307c217775fc5879e621aae62d90e4b1b02a
Ahmed Mahmoud <Ahmedumahmoud@gmail.com>
2023-06-25 09:00:39 +0200

    Fix ImageCopier

--- src/lib/ImageCopier.ts
+++ src/lib/ImageCopier.ts
@@ -4,7 +4,7 @@ import { toast } from "react-hot-toast";
 export async function copyImagesToClipboard([image]) {
   // const imageUrl = image;
   try {
-    const response = await fetch(image);
+    const response = await fetch(image, { headers: { Origin: 'https://dev.dipzin.com' } });
     const blob = await response.blob();
     const clipboardItem = new ClipboardItem({ 'image/png': blob });
     await navigator.clipboard.write([clipboardItem]);
