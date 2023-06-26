e508aba4ce5210b589c0b1a614b7870b63780579
Abdelrahman-Z <abdelrahmanziedan@gmail.com>
2023-06-26 21:22:57 +0300

    fix main nav

--- src/components/Stream.tsx
+++ src/components/Stream.tsx
@@ -25,6 +25,7 @@ const Stream: FC<StreamProps> = () => {
     setActiveControls('menu-search')
     return () => {
       setActiveView('')
+      setActiveControls('')
     }
   }, [])
   // 1. Initialize Stream and Page Platforms.
