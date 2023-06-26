f77a4ff9f2f443e8d9ca4140022a9136f0557cc6
Abdelrahman-Z <abdelrahmanziedan@gmail.com>
2023-06-26 19:23:50 +0300

    control the access page from middlware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,26 @@
 import { NextResponse, NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
+  const paths = ['/access' , '/profile']
   const url = request.nextUrl;
 
+  const token = request?.cookies?.get('token')?.value
+  for (const path of paths ) {
+    if(url.pathname.startsWith(path) && token){
+      return NextResponse.redirect(new URL('/' , url))
+    }
+  }
   if (url.pathname.startsWith('/access')) {
     const res = NextResponse.next();
     const invitationToken = url.searchParams.get("inv");
     const referralToken = url.searchParams.get("ref");
     const expires = new Date(Date.now() + (1000 * 60 * 60 * 24) * 60) // 60 days from the initial request
-
     if (invitationToken) {
       res.cookies.set('invitation-token', invitationToken, { expires })
     }
     if (referralToken) {
       res.cookies.set('referral-token', referralToken, { expires })
     }
-
     return res;
   }
 }
