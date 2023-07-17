3292de0cc651add97bb7726817523de09a7d90ba
Ahmed Mahmoud <Ahmedumahmoud@gmail.com>
2023-07-17 18:52:47 +0200

    Banner  & Navigator Updates

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,12 @@
+'use client'
+import { usePathname } from "next/navigation";
 
 const Banner = () => {
+    const path = usePathname();
+    const showPaths = ['/ios', '/android', '/web', '/search']
+    if (!showPaths.includes(path)) return
     return (
-        <div className="mx-auto rounded-[42px]">
+        <div className="mx-auto rounded-[42px] max-w-[92%]">
             <img
                 className="h-auto w-full"
                 src="/images/assets/banner.png"
