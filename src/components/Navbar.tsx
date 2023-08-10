4480a4092a78126c4d8a8766d0c92c39bd455d39
Abdelrhman Mahmoud <Abdelrhmanx74@gmail.com>
2023-08-10 11:22:58 +0300

    menu preview
    added logic for the menu button that shows the menu and added the platform switcher to it (still needs few tweeks)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,54 +1,67 @@
 'use client'
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link';
 import SparkleButton from './ui/SparkleButton';
 import { useAuth } from '@/lib/auth';
 import Navigator from './navigator/main';
 import { useResponsive } from '@/context/useResponsive';
+import OpenMenu from '@/components/navigator/main/menu';
+
 
 const Navbar: FC = () => {
-    const { user, loading } = useAuth();
-    return (
-        <header className="w-full flex justify-between fixed items-start text-white pt-8 px-5 lg:px-10 z-20 top-0 bg-gradient-to-b from-slate-950/80 to-slate-950/0">
-            <Link
-                href="/ios"
-                shallow
-                className="text-lg lg:text-2xl flex flex-col "
-            >
-                <Image
-                    className="mr-3 h-6 sm:h-9 w-auto inline"
-                    src="/images/assets/light-logo.svg"
-                    alt="Dipzin Logo"
-                    width={110}
-                    height={39}
-                    loader={({ src }) => src}
-                    unoptimized
-                />
-                <span className=' text-[9px] bg-gradient-to-r from-aqua-400 to-aqua-400/70 w-fit mt-1.5 leading-none text-aqua-950 px-1.5 py-1 rounded-[5px]'>BETA</span>
-            </Link>
+  const { user, loading } = useAuth();
+  return (
+    <header className="w-full flex justify-between fixed items-start text-white pt-8 px-5 lg:px-10 z-20 top-0 bg-gradient-to-b from-slate-950/80 to-slate-950/0">
+      <Link
+        href="/ios"
+        shallow
+        className="text-lg lg:text-2xl flex flex-col "
+      >
+        <Image
+          className="mr-3 h-6 sm:h-9 w-auto inline"
+          src="/images/assets/light-logo.svg"
+          alt="Dipzin Logo"
+          width={110}
+          height={39}
+          loader={({ src }) => src}
+          unoptimized
+        />
+        <span className=' text-[9px] bg-gradient-to-r from-aqua-400 to-aqua-400/70 w-fit mt-1.5 leading-none text-aqua-950 px-1.5 py-1 rounded-[5px]'>BETA</span>
+      </Link>
 
-            {!loading &&
-                <>
-                    <Navigator />
-                    <Menu user={user} />
-                </>
-            }
-
-
-        </header>
-    )
-}
-
+      {!loading && (
+        <>
+          <Navigator />
+          <Menu user={user} />
+        </>
+      )}
+    </header>
+  );
+};
 
 const Menu = (user) => {
-    const { isMobile } = useResponsive();
-    if (!isMobile) {
-        if (user.user) return <SparkleButton href='/pricing' >Unlock More!</SparkleButton>
-        return <SparkleButton href='/access' >Try it!</SparkleButton>
-    } else {
-        return <div className='ml-6'>Menu</div>
-    }
-}
+  const { isMobile } = useResponsive();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-export default Navbar
+  const handleMenuClick = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  if (!isMobile) {
+    if (user.user) return <SparkleButton href='/pricing' >Unlock More!</SparkleButton>;
+    return <SparkleButton href='/access' >Try it!</SparkleButton>;
+  } else {
+    return (
+      <div className='ml-6'>
+        <button onClick={handleMenuClick}>Menu</button>
+        {isMenuOpen && (
+          <div className='absolute top-0 left-0 w-full h-full'>
+            <OpenMenu />
+          </div>
+        )}
+      </div>
+    );
+  }
+};
+
+export default Navbar;
--- src/components/navigator/main/menu.tsx
+++ src/components/navigator/main/menu.tsx
@@ -1,3 +1,5 @@
+import PlatformSwitcher from '@/components/PlatformSwitcher'
+import { useResponsive } from '@/context/useResponsive'
 import { AnimatePresence, motion } from 'framer-motion'
 import Link from 'next/link'
 import { useState } from 'react'
@@ -5,6 +7,94 @@ import ReactPlayer from 'react-player'
 
 const Menu = () => {
     const [openVideo, setOpenVideo] = useState(false)
+  const { isMobile } = useResponsive();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  if(isMobile) {
+    return (
+        <motion.div
+            layout
+            key="menu"
+            className='overflow-x-hidden bg-[#050814] rounded-2xl p-8'
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0, height: 0, width: 0 }}
+        // transition={{ type: "spring", duration: 0.6, delay: 0.3 }}
+        > 
+             <div className='mb-4'>
+            <PlatformSwitcher/>
+            </div>
+            <div className='flex h-[90%] p-2 px-4 max-w-[800px] flex-col'>
+                <div className=' flex gap-x-2 mb-3 text-xs'>
+                    <Link href='/privacy' className=' text-slate-300  hover:text-aqua-500'>Privacy</Link>
+                    <Link href='/terms' className=' text-slate-300  hover:text-aqua-500'>Terms of Service</Link>
+                    <Link href='/copyrights' className=' text-slate-300  hover:text-aqua-500'>Copyrights</Link>
+                </div>
+                <div className=' flex gap-x-4 mb-2'>
+                    <div className=' flex gap-x-8'>
+                        <div className=' flex flex-col gap-y-2'>
+                            <p className=' text-slate-400'>Resources</p>
+                            <Resources />
+                            <Resources />
+                            <Resources />
+
+                        </div>
+                        <div className=' flex flex-col gap-y-2'>
+                            <p className=' text-slate-400'>Company</p>
+                            <Resources />
+                            <Resources />
+                            <Resources />
+                        </div>
+                    </div>
+                    <div className=' flex flex-col gap-3'>
+                        <p className=' text-slate-400'>Feature Release</p>
+                        <button className=' overflow-hidden' onClick={()=> setOpenVideo(true)}>
+                            <img src="/images/assets/video.svg" className=' w-[266px] h-[148px] -ml-11' alt="" />
+                        </button>
+                    </div>
+                </div>
+                <p className=' text-slate-500 text-xs'>Coming Soon</p>
+                <div className='overflow-hidden relative rounded-2xl w-full mt-2'>
+                    <div className='w-4 h-full  absolute left-0 bg-gradient-to-l from-slate-900/0 to-slate-900/90'></div>
+                    <div className='w-12 h-full absolute right-0 bg-gradient-to-r from-slate-900/0 to-slate-900/90'></div>
+                    <div className='flex space-x-2 touch-pan-x overflow-x-scroll w-[100%] h-full scrollbar-none'>
+                        <InitialSearchCard/>
+                        <InitialSearchCard/>
+                        <InitialSearchCard/>
+                        <InitialSearchCard/>
+                        <InitialSearchCard/>
+                        <InitialSearchCard/>
+                        
+                    </div>
+                </div>
+            </div>
+            <AnimatePresence>
+                {openVideo && (
+                <>
+                    <motion.div
+                    className=" fixed top-0 left-0 w-full h-full backdrop-blur-md bg-slate-900/70 z-50 flex items-center justify-center gap-8"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    >
+                    <ReactPlayer url={`https://www.youtube.com/watch?v=NkjXFMTln5Q`}
+                    className='z-[400] w-3/4 h-3/4'
+                    controls
+                    />
+                    
+                    <motion.div
+                        onClick={() => setOpenVideo(false)}
+                        className={
+                        "w-[100%] h-[100%] fixed top-0 left-0 bg-transparent"
+                        }
+                    ></motion.div>
+                    </motion.div>
+                </>
+                )}
+            </AnimatePresence>
+        </motion.div>
+    )
+  }
+
     return (
         <motion.div
             layout
