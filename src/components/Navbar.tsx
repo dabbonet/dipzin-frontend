b8f17d1caa59d25241b4d9e76b8e824e4839c679
Abdelrhman Mahmoud <Abdelrhmanx74@gmail.com>
2023-08-12 08:08:05 +0300

    responsive menu update no.2
    added a close button to the menu, change the menu file to be in the Navbar.tsx, now it a functional menu sidbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,8 +6,9 @@ import SparkleButton from './ui/SparkleButton';
 import { useAuth } from '@/lib/auth';
 import Navigator from './navigator/main';
 import { useResponsive } from '@/context/useResponsive';
-import OpenMenu from '@/components/navigator/main/mobileMenu';
-
+import PlatformSwitcher from '@/components/PlatformSwitcher'
+import { AnimatePresence, motion } from 'framer-motion'
+import ReactPlayer from 'react-player'
 
 const Navbar: FC = () => {
   const { user, loading } = useAuth();
@@ -41,28 +42,144 @@ const Navbar: FC = () => {
 };
 
 const Menu = (user) => {
+  const [openVideo, setOpenVideo] = useState(false);
   const { isMobile } = useResponsive();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const handleMenuClick = () => {
+ const handleMenuClick = (e) => {
+  e.preventDefault();
     setIsMenuOpen(!isMenuOpen);
   };
+  const handleCloseClick = (e) => {
+      e.preventDefault();
+    setIsMenuOpen(false);
+  }
 
   if (!isMobile) {
     if (user.user) return <SparkleButton href='/pricing' >Unlock More!</SparkleButton>;
     return <SparkleButton href='/access' >Try it!</SparkleButton>;
-  } else {
+  } else if(!isMenuOpen) {
     return (
       <div className='ml-6'>
         <button onClick={handleMenuClick}>Menu</button>
-        {isMenuOpen && (
-          <div className='absolute top-0 left-0 w-full h-full'>
-            <OpenMenu />
           </div>
-        )}
-      </div>
     );
+  }else{
+    return (
+          <motion.div
+          layout
+          key="menu"
+          className=" overflow-x-hidden bg-[#050814] rounded-2xl p-8 fixed top-0 right-0 left-0 w-full h-full items-center justify-center"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0, height: 0, width: 0 }}
+          >
+        
+          <button onClick={handleCloseClick}><img className=' ' src='/images/assets/arrow_back.svg' alt='Close Menu'/>
+                    </button>
+
+       <div onClick={handleCloseClick}  className=' absolute  top-0 right-0 left-0 mt-8 '>
+                 <PlatformSwitcher />
+        </div>
+       <div onClick={handleCloseClick} className="absolute top-0 right-0 mr-8 mt-8  p-1">
+            <SparkleButton  href={user.user ? '/pricing' : '/access'}>
+                {user.user ? 'Unlock More!' : 'Try it!'}
+            </SparkleButton>
+        </div>
+
+            <div onClick={handleCloseClick} className='flex h-[90%] p-2 px-4 max-w-[800px] flex-col mt-10'>
+                <div className=' flex gap-x-4 mb-2'>
+                    <div className=' flex gap-x-8'>
+                        <div className=' flex flex-col gap-y-2'>
+                            <p className=' text-slate-400'>Resources</p>
+                            <Resources />
+                            <Resources />
+                            <Resources />
+
+                        </div>
+                        <div className=' flex flex-col gap-y-2'>
+                            <p className=' text-slate-400'>Company</p>
+                            <Resources />
+                            <Resources />
+                            <Resources />
+                        </div>
+                    </div>
+                    <div className=' flex flex-col gap-3'>
+                        <p className=' text-slate-400'>Feature Release</p>
+                        <button className=' overflow-hidden' onClick={()=> setOpenVideo(true)}>
+                            <img src="/images/assets/video.svg" className=' w-[266px] h-[148px] -ml-0' alt="" />
+                        </button>
+                    </div>
+                </div>
+                <p className=' text-slate-500 text-xs'>Coming Soon</p>
+                <div className='overflow-hidden relative rounded-2xl w-full mt-2'>
+                    <div className='w-4 h-full  absolute left-0 bg-gradient-to-l from-slate-900/0 to-slate-900/90'></div>
+                    <div className='w-12 h-full absolute right-0 bg-gradient-to-r from-slate-900/0 to-slate-900/90'></div>
+                    <div className='flex space-x-2 touch-pan-x overflow-x-scroll w-[100%] h-full scrollbar-none'>
+                        <InitialSearchCard/>
+                        <InitialSearchCard/>
+                        <InitialSearchCard/>
+                        <InitialSearchCard/>
+                        <InitialSearchCard/>
+                        <InitialSearchCard/>
+                    </div>
+                </div>
+                 <hr className='mt-6'/>
+              <div  className=' flex gap-x-2 mt-3 text-xs justify-center'>
+                    <Link href='/privacy'  className=' text-slate-300  hover:text-aqua-500'>Privacy</Link>
+                    <Link href='/terms' className=' text-slate-300  hover:text-aqua-500'>Terms of Service</Link>
+                    <Link href='/copyrights' className=' text-slate-300  hover:text-aqua-500'>Copyrights</Link>
+                </div>
+            </div>
+
+            <AnimatePresence>
+                {openVideo && (
+                <>
+                    <motion.div
+                    className=" fixed top-0 left-0 w-full h-full backdrop-blur-md bg-slate-900/70 z-50 flex items-center justify-center gap-8"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    >
+                    <ReactPlayer url={`https://www.youtube.com/watch?v=NkjXFMTln5Q`}
+                    className='z-[400] w-3/4 h-3/4'
+                    controls
+                    />
+                    
+                    <motion.div
+                        onClick={() => setOpenVideo(false)}
+                        className={
+                        "w-[100%] h-[100%] fixed top-0 left-0 bg-transparent"
+                        }
+                    ></motion.div>
+                    
+                    </motion.div>
+                </>
+                )}
+            </AnimatePresence>
+        </motion.div>
+    )
+}
   }
-};
 
 export default Navbar;
+
+const InitialSearchCard = ()=> {
+    return <div className='flex-shrink-0 w-[300px] bg-slate-900 hover:bg-slate-800 rounded-2xl p-4 gap-1'>
+    <h3 className='text-slate-200 font-semibold'>Figma Plugin</h3>
+    <p className='text-slate-400 text-xs'>Work from your browser with our lightweight extension.</p>
+    <span className=' bg-lime-100 rounded-md  px-1 text-lime-900'>In Progress</span>
+</div>
+}
+
+const Resources = () => {
+    return (
+        <div className=' flex items-start gap-x-2'>
+            <img className="mt-2 w-4 text-white" src="/images/assets/refresh.svg" alt="refresh" />
+            <div>
+                <Link href='/' className='text-base hover:text-aqua-500'>Stream</Link>
+                <p className=' text-slate-400 text-[10px]'>The main gate to discover dipzin <br /> platform.</p>
+            </div>
+        </div>
+    )
+    }
