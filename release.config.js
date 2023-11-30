cdaf595c1cc0ccdbf257fdee120de937c8cf4ec5
Ahmed Mahmoud <Ahmedumahmoud@gmail.com>
2023-11-30 11:10:11 +0200

    ci: Semantic + Build

--- release.config.js
+++ release.config.js
@@ -7,7 +7,14 @@ const config = {
         "assets": ["dist/*.js", "dist/*.js.map"],
         "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
       }],
-      '@semantic-release/github'
+      [
+        "@semantic-release/github",
+        {
+          "successComment": false,
+          "failTitle": false,
+          "failComment": false
+        }
+      ]
     ]
   };
   
