87ac7c096c2676b2f9e8ffcf10cd5d72563ae2b0
Ahmed Mahmoud <Ahmedumahmoud@gmail.com>
2024-04-16 21:41:25 +0200

    initial structuring, refactor screenActions.

--- src/app/(static)/app/[platform]/[slug]/ScreenActions.tsx
+++ src/app/(static)/app/[platform]/[slug]/ScreenActions.tsx
@@ -1,79 +1,36 @@
+// /search/components/ScreenActions.tsx
 "use client";
+import { FC } from "react";
 import { ActionBar, SquareButton } from "@/components/ActionBar";
 import Icons from "@/components/Icons";
-import SoonToast from "@/components/SoonToast";
 import { useDialog } from "@/context/useDialog";
-import { copyImagesToClipboard } from "@/lib/ImageCopier";
-import { downloadImage } from "@/lib/ImageDownloader";
-import { getUser } from "@/lib/auth";
-import { navigatorProps } from "@/lib/types/screenActions";
-import { getAssetsURL } from "@/lib/utils";
-import { FC } from "react";
-import toast from "react-hot-toast";
-type button = {
-  title?: any
-  icon?: any
-  handler?: any
-  appName?: any
-  screen?: any
-}
-const ButtonWrapper = ({ title, icon, handler, appName, screen }: button) => {
-  const { showDialog, DIALOG_ENUM } = useDialog();
-  if (appName && screen) {
-    const screenName = screen?.attributes?.screen?.data?.attributes || screen;
-    if (screenName) {
-      if (appName || screen?.attributes?.order) {
-        const name = screenName?.hash + screenName?.ext || screen;
-        return (
-          <SquareButton
-            onClick={async () => {
-              const isUserAuth = await getUser();
-                console.log(isUserAuth)
-                if (!isUserAuth) {
-                  // showDialog(DIALOG_ENUM.UPGRADE_AD,'Upgrade and get access to exclusive features');
-                  showDialog(DIALOG_ENUM.ACCESS,'Login to use this features');
-                  return
-                } 
-                // await setTimeout(() => {
-                  if (title === "Copy PNG" || title === "Copy Link"){
-                    handler(getAssetsURL(name));
-                  }else{
-                    handler(name, appName + " " + screen?.attributes?.order || screen);
-                  }
-                // }, 5000);
+import useScreenActions from '@/hooks/useScreenActions';
 
-              }}
-              
-                >
-            <SquareButton.Title className="w-[80%]">{title}</SquareButton.Title>
-            <SquareButton.Icon>{icon}</SquareButton.Icon>
-          </SquareButton>
-        );
-      }
-    }
-  }
+type ButtonProps = {
+  title: string;
+  icon: JSX.Element;
+  handler: (urlOrName: string, additionalInfo?: string) => void;
+  screenInfo: {
+    appName: string;
+    screen: any; // Specify more accurate types as needed
+  };
 };
 
-const ScreenActions: FC<navigatorProps> = ({ appName, screen }) => {
-  const handleLikeScreen = () => {
-    toast.remove();
-    toast.custom(<SoonToast />, { duration: 2000 });
-  };
+const ButtonWrapper: FC<ButtonProps> = ({ title, icon, handler, screenInfo }) => {
+  const { screen, appName } = screenInfo;
+  const { showDialog, DIALOG_ENUM } = useDialog();
+  const screenURL = screen.attributes?.screen?.data?.attributes?.url || screen.url; // Adapt as necessary based on data structure
 
-  const handleDownloadScreen = async (name, fileName) => {
-    if (name) {
-      downloadImage(fileName, name);
-    }
-  };
+  return (
+    <SquareButton onClick={() => handler(screenURL, `${appName} ${screen.attributes?.order}`)}>
+      <SquareButton.Title>{title}</SquareButton.Title>
+      <SquareButton.Icon>{icon}</SquareButton.Icon>
+    </SquareButton>
+  );
+};
 
-  const handleCopyPng = async (screenURL) => {
-    await copyImagesToClipboard([screenURL]);
-  };
-
-  const handleCopyLink = async (screenURL) => {
-    navigator.clipboard.writeText(screenURL);
-    toast.success("App Link Copied.");
-  };
+const ScreenActions: FC<{ appName: string; screen: any }> = ({ appName, screen }) => {
+  const { handleDownloadScreen, handleCopyLink, handleCopyPng, handleLikeScreen } = useScreenActions();
 
   return (
     <ActionBar className="z-50 flex flex-col fixed sm:right-10 right-0 top-[32%] w-fit h-auto">
@@ -81,33 +38,28 @@ const ScreenActions: FC<navigatorProps> = ({ appName, screen }) => {
         title="Like Screen"
         icon={<Icons.Heart />}
         handler={handleLikeScreen}
-        appName={appName}
-        screen={screen}
+        screenInfo={{ appName, screen }}
       />
       <ButtonWrapper
         title="Download"
         icon={<Icons.Download />}
         handler={handleDownloadScreen}
-        appName={appName}
-        screen={screen}
+        screenInfo={{ appName, screen }}
       />
       <ButtonWrapper
         title="Copy PNG"
         icon={<Icons.Copy />}
         handler={handleCopyPng}
-        appName={appName}
-        screen={screen}
+        screenInfo={{ appName, screen }}
       />
       <ButtonWrapper
         title="Copy Link"
         icon={<Icons.CopyFilled />}
         handler={handleCopyLink}
-        appName={appName}
-        screen={screen}
+        screenInfo={{ appName, screen }}
       />
     </ActionBar>
   );
 };
 
-export { ButtonWrapper };
 export default ScreenActions;
