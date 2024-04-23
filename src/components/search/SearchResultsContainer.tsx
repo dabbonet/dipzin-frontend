29e139427001e122e04a0bb7e147e8e4cf6bc0b1
abdelrahman kamal <kamalitfahmy2002@gmail.com>
2024-04-23 16:57:23 +0200

    fix: keyboard navigation not working

--- src/components/search/SearchResultsContainer.tsx
+++ src/components/search/SearchResultsContainer.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { cn } from "@/lib/utils";
 import { VirtuosoGrid } from "react-virtuoso";
@@ -6,58 +6,62 @@ import SingleScreen from "../screen/SingleScreen";
 import { useSearchContext } from "@/context/SearchContext";
 import { usePlatform } from "@/context/usePlatforms";
 import StreamLoader from "../StreamLoader";
+import { useKeyboardNavigation } from '@/hooks/useKeyboardNavigation'
 
 export const SearchResultsContainer = () => {
-    const { setOpenScreen, data, isLoading, loadMore } = useSearchContext();
-    const { selected } = usePlatform();
+  const { openScreen,setOpenScreen, data, isLoading, loadMore } = useSearchContext();
+  const { selected } = usePlatform();
+  const {
+    openScreenByIndex,
+  } = useKeyboardNavigation(data, openScreen, setOpenScreen);
 
-    if (!isLoading && data?.length === 0) return <div className=' w-full h-full flex justify-center items-center'>there is no screens with this filters</div>
-
+  if (!isLoading && data?.length === 0)
     return (
-        <>
-            {data !== null && data?.length !== 0 ? <VirtuosoGrid
-                className="mt-6 max-w-[90%] mx-auto"
-                useWindowScroll
-                endReached={loadMore}
-                data={data && data}
-                style={{ minHeight: 100, width: "100%" }}
-                listClassName={cn(
-                    "grid content-center gap-6 pt-0 grid-cols-2",
-                    +selected === 3
-                        ? "2xl:grid-cols-3 md:grid-cols-3"
-                        : " 2xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-4"
-                )}
-                totalCount={data && data?.length}
-                overscan={10}
-                itemContent={(index, data) => {
-                    return (
-                        <SingleScreen screen={data?.screen} appName={data.app.name} tagLine={data.app.tag_line} setOpen={() => setOpenScreen(data)} icon={data.app.icon} />
-                    );
-                }}
-                components={{
-                    Footer: () => {
-                        return (
-                            <>
-                                <div
-                                    className="pt-10 pb-48 text-center text-slate-500"
-                                >
-                                    {isLoading &&
-                                        "Loading More"
-                                    }
-                                    {!isLoading &&
-                                        "End Reached"
-                                    }
-                                </div>
-                            </>
-                        )
-                    },
-                }}
-            />
-                :
-                <div className="w-[92%] h-full">
-                    <StreamLoader />
-                </div>
-            }
-        </>
-    )
-}
+      <div className=" w-full h-full flex justify-center items-center">
+        there is no screens with this filters
+      </div>
+    );
+
+  return (
+    <>
+      {data !== null && data?.length !== 0 ? (
+        <VirtuosoGrid
+          className="mt-6 max-w-[90%] mx-auto"
+          useWindowScroll
+          endReached={loadMore}
+          data={data && data}
+          style={{ minHeight: 100, width: "100%" }}
+          listClassName={cn(
+            "grid content-center gap-6 pt-0 grid-cols-2",
+            +selected === 3
+              ? "2xl:grid-cols-3 md:grid-cols-3"
+              : " 2xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-4"
+          )}
+          totalCount={data && data?.length}
+          overscan={10}
+          itemContent={(index, data) => {
+            return (
+              <SingleScreen screen={data?.screen} appName={data.app.name} tagLine={data.app.tag_line} setOpen={() => { setOpenScreen(data); openScreenByIndex(index) }} icon={data.app.icon} />
+            );
+          }}
+          components={{
+            Footer: () => {
+              return (
+                <>
+                  <div className="pt-10 pb-48 text-center text-slate-500">
+                    {isLoading && "Loading More"}
+                    {!isLoading && "End Reached"}
+                  </div>
+                </>
+              );
+            },
+          }}
+        />
+      ) : (
+        <div className="w-[92%] h-full">
+          <StreamLoader />
+        </div>
+      )}
+    </>
+  );
+};
