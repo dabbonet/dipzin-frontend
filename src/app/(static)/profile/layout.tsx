6341c951981065f26f419cd7e4669e0ae6fb5e15
Ahmed Mahmoud <Ahmedumahmoud@gmail.com>
2024-08-13 18:45:04 +0300

    refactor: remove old codebase

--- src/app/(static)/profile/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function Layout({ children }) {
-    return <>
-        <div className="flex justify-center items-center min-h-[80vh]">
-            {children}
-        </div>
-    </>
-}
-
-
--- src/app/api/user/connect/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET(request: Request) {
-    const { searchParams } = new URL(request.url);
-    const provider = searchParams.get('provider');
-    return NextResponse.redirect('https://rah.dipzin.com/api/connect/' + provider)
-}
