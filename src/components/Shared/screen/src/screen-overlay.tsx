c5b636fedbfec0981496eb73450c95cc88237171
Abdelrhman Mahmoud <118855701+Abdelrhmanx74@users.noreply.github.com>
2024-10-23 21:33:42 +0300

    style(panel): screen & app panel ui enhancments

--- src/components/Shared/screen/src/screen-overlay.tsx
+++ src/components/Shared/screen/src/screen-overlay.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { motion } from 'framer-motion'
 import {
   Avatar, AvatarImage, AvatarFallback
 } from '@/components/Shared/avatar'
@@ -11,6 +10,7 @@ import { Checkbox } from '@/components/UI/checkbox'
 import { storage } from '@/utils/storage'
 import type { ScreenType } from '@/types/app-types'
 
+// App Info Component
 const AppInfo = ({ app }: { app: ScreenType['screen']['app'] }) => (
   <div className="flex items-center gap-4">
     <Avatar>
@@ -24,6 +24,7 @@ const AppInfo = ({ app }: { app: ScreenType['screen']['app'] }) => (
   </div>
 )
 
+// Global Top Overlay
 const GlobalTopOverlay = () => (
   <div className="w-full h-fit flex items-center justify-between px-5">
     <Checkbox className="size-[28px] rounded-[0.4rem] border-[2.5px]" />
@@ -37,56 +38,35 @@ const GlobalTopOverlay = () => (
           <Button size="md" className="rounded-full p-2 bg-slate-800" variant="darkGray" isIconOnly>
             <Icon.EllipsisHorizontal className="size-6 text-white" />
           </Button>
-    )}
+        )}
         content="content"
         classNames={{
           content: 'w-fit',
         }}
         placement="end"
       />
-      {' '}
-
     </div>
   </div>
 )
 
-const TopOverlay = () => {
-  const overlayVariants = {
-    initial: { opacity: 0 },
-    hover: { opacity: 1 },
-  }
+// Top Overlay Component
+const TopOverlay = ({ app }: { app: ScreenType['screen']['app'] }) => (
+  <div className={`absolute top-0 inset-x-0 flex ${app?.platform === 'web' ? 'pt-4 pb-[70px]' : 'pt-6 pb-[93px]'} items-center justify-center gap-4 bg-screen-hover-gradient-to-bottom opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out`}>
+    <GlobalTopOverlay />
+  </div>
+)
 
-  return (
-    <motion.div
-      className="absolute top-0 inset-x-0 flex pt-6 pb-[93px] items-center justify-center gap-4 bg-screen-hover-gradient-to-bottom"
-      variants={overlayVariants}
-      transition={{ duration: 0.4, ease: 'easeInOut' }}
-    >
-      <GlobalTopOverlay />
-    </motion.div>
-  )
-}
+// Bottom Overlay Component
+const BottomOverlay = ({ app }: { app: ScreenType['screen']['app'] }) => (
+  <div className={`absolute bottom-0 inset-x-0 flex px-4 ${app?.platform === 'web' ? 'pb-[20px] pt-[36px]' : 'pb-[26px] pt-[48px]'} items-center justify-between bg-screen-hover-gradient-to-top opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out`}>
+    <AppInfo app={app} />
+  </div>
+)
 
-const BottomOverlay = ({ app }: { app: ScreenType['screen']['app'] }) => {
-  const overlayVariants = {
-    initial: { opacity: 0 },
-    hover: { opacity: 1 },
-  }
-
-  return (
-    <motion.div
-      className="absolute bottom-0 inset-x-0 flex px-4 pb-[26px] pt-[48px] items-center justify-between bg-screen-hover-gradient-to-top"
-      variants={overlayVariants}
-      transition={{ duration: 0.4, ease: 'easeInOut' }}
-    >
-      <AppInfo app={app} />
-    </motion.div>
-  )
-}
-
+// Screen Overlay component that triggers hover animation
 export const ScreenOverlay = ({ app }: { app: ScreenType['screen']['app'] }) => (
   <>
-    <TopOverlay />
+    <TopOverlay app={app} />
     <BottomOverlay app={app} />
   </>
 )
