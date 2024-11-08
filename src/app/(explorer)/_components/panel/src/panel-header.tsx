d63a188b3bf5c748c65f463a5956e9cbb773651e
Abdelrhman Mahmoud <118855701+Abdelrhmanx74@users.noreply.github.com>
2024-11-08 21:18:45 +0200

    style(grid header): grid header ui optimization

--- src/app/(explorer)/_components/panel/src/panel-header.tsx
+++ src/app/(explorer)/_components/panel/src/panel-header.tsx
@@ -2,20 +2,22 @@
 
 import { Button } from '@/components/Shared/button';
 import { Icon } from '@/components/UI/icon';
+import { Skeleton } from '@/components/UI/skeleton';
 import { capitalizeFirstLetter } from '@/utils/StringUtils';
 import { useSession } from 'next-auth/react';
 import React from 'react';
 
 const PanelHeader: React.FC = () => {
-  const session = useSession()
-  const user = session?.data?.user
+  const { data: session, status } = useSession();
+  const user = session?.user;
 
   return (
-    <div className="w-full  h-fit flex items-center justify-between gap-3 mb-6">
-      <h1 className="text-2xl sm:text-5xl font-semibold text-slate-300 font-outfit">
+    <div className="w-full h-fit flex items-center justify-between gap-3 mt-4 mb-6">
+      <h1 className="text-2xl sm:text-[2.5rem] font-semibold text-slate-300 font-outfit flex items-center">
         Welcome,
-        {' '}
-        {!user ? "Guest" : capitalizeFirstLetter(user?.name ?? '...')}
+        {status === 'loading' && <Skeleton className="w-32 h-8 bg-slate-800 ml-2" />}
+        {status === 'authenticated' && <span className="ml-2">{capitalizeFirstLetter(user?.name ?? 'User')}</span>}
+        {status !== 'loading' && status !== 'authenticated' && <span className="ml-2">Guest</span>}
       </h1>
       <div className="size-fit flex items-center gap-4">
         <Button href="/collections" variant="darkGray">
